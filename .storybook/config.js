fa24c29fc3ce1d75e1b9f8e600347ec4fdc4f1c2
Scott Dickerson <scottsd@us.ibm.com>
2019-01-04 16:33:34 -0600

    fix(a11y-addon): add a11y addon to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { checkA11y } from 'storybook-addon-a11y';
+import { checkA11y } from '@storybook/addon-a11y';
+
 import Container from './Container';
 
-// addDecorator(checkA11y);
+addDecorator(checkA11y);
 // addDecorator(story => <Container story={story} />);
 addDecorator(withInfo);
 
