7b45b799977b587c0ef9b60039e7d7802b621985
Scott Dickerson <scottsd@us.ibm.com>
2019-01-17 13:23:57 -0600

    fix(wizardmodal): add default index files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export ButtonEnhanced from './components/ButtonEnhanced';
 export AddCard from './components/AddCard';
 export BaseModal from './components/BaseModal';
+export WizardModal from './components/WizardModal';
