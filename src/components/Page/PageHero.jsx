c5f8240b842a2ce91268f823a42d6d74acdd355d
Scott Dickerson <swiftfoot@hotmail.com>
2019-04-03 15:27:03 -0500

    test(progress): add testcases for progress indicator

--- src/components/Page/PageHero.jsx
+++ src/components/Page/PageHero.jsx
@@ -97,7 +97,7 @@ const StyledPageBlurb = styled.p`
 /**
  * Renders the hero text and styles for the page.  Can either render Section and Title with blurb or support a narrow one with breadcrumbs.
  */
-const Page = ({ section, title, blurb, className, crumb, rightContent, switchers }) => (
+const PageHero = ({ section, title, blurb, className, crumb, rightContent, switchers }) => (
   <StyledPageHero className={className}>
     {crumb || (
       <div>
@@ -112,7 +112,7 @@ const Page = ({ section, title, blurb, className, crumb, rightContent, switchers
   </StyledPageHero>
 );
 
-Page.propTypes = propTypes;
-Page.defaultProps = defaultProps;
+PageHero.propTypes = propTypes;
+PageHero.defaultProps = defaultProps;
 
-export default Page;
+export default PageHero;
