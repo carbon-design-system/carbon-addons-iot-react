5dc0e88c05a2b5e50dd7a92a24ecdbaeeb45c020
Tom Klapiscak <klapitom@uk.ibm.com>
2019-04-04 17:54:37 +0100

    feat(table): add filtering support to AsyncTable

--- src/components/Table/AsyncTable.jsx
+++ src/components/Table/AsyncTable.jsx
@@ -25,32 +25,50 @@ import {
 */
 
 import * as baseTableActions from './tableActionCreators';
-console.log(baseTableActions);
 
 class MockApiClient {
   static totalRows = 100;
+  static firstNames = ['Tom', 'Amy', 'Bryan', 'Cynthia', 'Claudia', 'Denny', 'Mats', 'Luaithrenn', 'Scott', 'Taylor']
+  static lastNames  = ['Smith', 'Brown', 'Johnson', 'Williams', 'Miller', 'Davis', 'Wilson', ]
 
-  getData = (offset, limit) => {
+  constructor() {
+    this.data = [];
+
+    for (let i = 0; i < MockApiClient.totalRows; i += 1) {
+      this.data = [
+        ...this.data,
+        {
+          firstName: `${MockApiClient.firstNames[Math.floor(Math.random()*MockApiClient.firstNames.length)]} (${i + 1})`,
+          lastName:  `${MockApiClient.lastNames [Math.floor(Math.random()*MockApiClient.lastNames.length )]} (${i + 1})`,
+        },
+      ]
+    }
+  }
+
+  getData = (offset, limit, firstName = undefined, lastName = undefined) => {
+    console.log('Fetching ', offset, limit, firstName, lastName);
+
     return new Promise((resolve, reject) => {
-      // cap results to totalRows even if more are requested
-      const maxRow = Math.min(MockApiClient.totalRows, offset + limit);
 
-      let results = [];
-      for (let i = offset; i < maxRow; i += 1) {
-        results = [
-          ...results,
-          {
-            firstName: `${i + 1} first name`,
-            lastName: `${i + 1} last name`,
-          },
-        ];
-      }
+      // filter results
+      const filteredData = this.data
+        .filter(
+          d => firstName === undefined || `${d.firstName.toLowerCase()}`.includes(firstName.toLowerCase())
+        )
+        .filter(
+          d => lastName === undefined || `${d.lastName.toLowerCase()}`.includes(lastName.toLowerCase())
+        )
+
+      // cap results to total (matching) rows even if more are requested
+      const maxRow = Math.min(filteredData.length, offset + limit);
+
+      const results = filteredData.slice(offset, maxRow);
 
       setTimeout(
         () =>
           resolve({
             meta: {
-              totalRows: MockApiClient.totalRows,
+              totalRows: filteredData.length,
             },
             results: results,
           }),
@@ -63,36 +81,56 @@ class MockApiClient {
 const reducer = (state, action) => {
   switch (action.type) {
     case baseTableActions.TABLE_REGISTER:
-      return update(state, {
-        data: {
-          $set: action.payload,
-        },
-        view: {
-          pagination: {
-            totalItems: { $set: action.totalItems },
+      return update(
+        baseTableReducer(state, action),
+        {
+          data: {
+            $set: action.payload,
           },
-        },
-      });
+          view: {
+            pagination: {
+              totalItems: { $set: action.totalItems },
+            },
+          },
+        }
+      );
+
+    // clear all loaded data (and reset totalItems) if filter values change
+    case baseTableActions.TABLE_FILTER_CLEAR:
+    case baseTableActions.TABLE_FILTER_APPLY:
+      return update(
+        baseTableReducer(state, action),
+        {
+          data: {
+            $set: []
+          },
+          view: {
+            pagination: {
+              totalItems: { $set: undefined },
+            },
+          },
+        }
+      );
+    
     default:
       return baseTableReducer(state, action);
   }
 };
 
+const apiClient = new MockApiClient();
+
 const AsyncTable = ({}) => {
-  const apiClient = new MockApiClient();
+  
+
+  const columns = [
+    { id: 'firstName', name: 'First Name' },
+    { id: 'lastName', name: 'Last Name' }
+  ];
 
   const [state, dispatch] = useReducer(reducer, {
     data: [],
     view: {
       filters: [
-        {
-          columnId: 'string',
-          value: 'whiteboard',
-        },
-        {
-          columnId: 'select',
-          value: 'option-B',
-        },
       ],
       pagination: {
         pageSize: 10,
@@ -111,7 +149,6 @@ const AsyncTable = ({}) => {
         },
       },
       toolbar: {
-        activeBar: 'filter',
         batchActions: [
           {
             id: 'delete',
@@ -127,41 +164,63 @@ const AsyncTable = ({}) => {
     },
   });
 
+
   // This hook is responsible for refetching more data asynchronously
   // as necessary when the page is changed
-  useEffect(() => {
-    const page = state.view.pagination.page;
-    const pageSize = state.view.pagination.pageSize;
+  useEffect(
+    () => {
+      // console.log('pagination', state.view.pagination);
+      // console.log('filters', state.view.filters);
 
-    const requestedFrom = (page - 1) * pageSize;
+      let firstNameFilterValue, lastNameFilterValue;
+      if (state.view.filters) {
+        const firstNameFilter = state.view.filters.find(f => f.columnId === 'firstName');
+        firstNameFilterValue = firstNameFilter ? firstNameFilter.value : undefined;
 
-    // cap by totalItems to avoid unnecessarily reloading the final page in cases where totalItems % pageSize > 0
-    // (unless this is the first fetch - in which case totalItems will be undefined)
-    const requestedUpTo =
-      state.view.pagination.totalItems === undefined
-        ? requestedFrom + pageSize
-        : Math.min(requestedFrom + pageSize, state.view.pagination.totalItems);
+        const lastNameFilter = state.view.filters.find(f => f.columnId === 'lastName');
+        lastNameFilterValue = lastNameFilter ? lastNameFilter.value : undefined;    
+      }
 
-    // do we need to load more data?
-    const loadedUpTo = state.data.length;
 
-    const remainingToFetch = requestedUpTo - loadedUpTo;
+      const page = state.view.pagination.page;
+      const pageSize = state.view.pagination.pageSize;
 
-    if (remainingToFetch > 0) {
-      dispatch(baseTableActions.tableLoadingSet(true, remainingToFetch));
+      const requestedFrom = (page - 1) * pageSize;
 
-      apiClient.getData(loadedUpTo, remainingToFetch).then(data => {
-        const tableData = data.results.map((r, idx) => ({ id: `${loadedUpTo + idx}`, values: r }));
-        dispatch(
-          baseTableActions.tableRegister([...state.data, ...tableData], data.meta.totalRows)
-        );
-        dispatch(baseTableActions.tableLoadingSet(false));
-      });
-    } else {
-    }
-  }, [state.view.pagination.page, state.view.pagination.pageSize]);
+      // cap by totalItems to avoid unnecessarily reloading the final page in cases where totalItems % pageSize > 0
+      // (unless this is the first fetch - in which case totalItems will be undefined)
+      const requestedUpTo =
+        state.view.pagination.totalItems === undefined
+          ? requestedFrom + pageSize
+          : Math.min(requestedFrom + pageSize, state.view.pagination.totalItems);
 
-  //console.log(state);
+      // do we need to load more data?
+      const loadedUpTo = state.data.length;
+
+      const remainingToFetch = requestedUpTo - loadedUpTo;
+
+      if (remainingToFetch > 0) {
+        dispatch(baseTableActions.tableLoadingSet(true, remainingToFetch));
+
+        apiClient.getData(
+          loadedUpTo,
+          remainingToFetch, 
+          firstNameFilterValue,
+          lastNameFilterValue,
+        ).then(data => {
+          const tableData = data.results.map((r, idx) => ({ id: `${loadedUpTo + idx}`, values: r }));
+          dispatch(
+            baseTableActions.tableRegister([...state.data, ...tableData], data.meta.totalRows)
+          );
+          dispatch(baseTableActions.tableLoadingSet(false));
+        });
+      } else {
+      }
+    },
+    [state.view.pagination.page, state.view.pagination.pageSize, state.view.filters]
+  );
+
+  // console.log(state);
 
   const actions = {
     pagination: {
@@ -220,17 +279,17 @@ const AsyncTable = ({}) => {
 
   return (
     <Table
-      columns={[{ id: 'firstName', name: 'First Name' }, { id: 'lastName', name: 'Last Name' }]}
+      columns={columns}
       data={state.data}
       view={state.view}
       actions={actions}
       options={{
         hasFilter: true,
-        hasSearch: true,
+        hasSearch: false,
         hasPagination: true,
         hasRowSelection: true,
-        hasRowExpansion: true,
-        hasRowActions: true,
+        hasRowExpansion: false,
+        hasRowActions: false,
         hasColumnSelection: true,
         shouldExpandOnRowClick: false,
       }}
