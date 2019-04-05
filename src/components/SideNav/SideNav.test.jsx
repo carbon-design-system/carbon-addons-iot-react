57713dd524fc5cbd6d3c80040cf5c54db3541305
David Conner <rdconner@us.ibm.com>
2019-04-04 21:06:58 -0400

    feat(sideNav):add test cases for sidenav

--- src/components/SideNav/SideNav.test.jsx
+++ src/components/SideNav/SideNav.test.jsx
@@ -1,62 +1,113 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { Icon } from 'carbon-components-react';
+import { mount, render } from 'enzyme';
+import AppSwitcher from '@carbon/icons-react/lib/app-switcher/24';
+import Chip from '@carbon/icons-react/lib/chip/24';
+import Group from '@carbon/icons-react/lib/group/24';
 
-import Header from '../Header';
+import SideNav from './SideNav';
+import CarbonSideNav from './CarbonSideNav';
 
 React.Fragment = ({ children }) => children;
 
-describe('Header testcases', () => {
+describe('SideNav testcases', () => {
   // it.skip('skip this suite', () => {});
-  it('should render', () => {
-    const onClick = jest.fn();
-    const actionItems = [
-      {
-        label: 'alerts',
-        onClick,
-        btnContent: <Icon name="notification-on" fill="white" description="Icon" />,
-      },
-      {
-        label: 'help',
-        onClick,
-        btnContent: (
-          <Icon
-            name="header--help"
-            fill="white"
-            description="Icon"
-            className="bx--header__menu-item bx--header__menu-title"
-          />
-        ),
-        childContent: [
-          {
-            onCLick: () => console.log('hi'),
-            content: <p>This is a link</p>,
-          },
-          {
-            onCLick: () => console.log('hi'),
-            content: (
-              <React.Fragment>
-                <span>
-                  JohnDoe@ibm.com
-                  <Icon name="header--avatar" fill="white" description="Icon" />
-                </span>
-              </React.Fragment>
-            ),
-          },
-        ],
-      },
-    ];
+  /* eslint-disable */
+  const links = [
+    {
+      label: 'Boards',
+      icon: (
+        <AppSwitcher
+          fill="white"
+          description="Icon"
+          className="bx--header__menu-item bx--header__menu-title"
+        />
+      ),
+      onClick: jest.fn(),
+      href: 'javascript:void(0)',
+      linkContent: 'Boards',
+    },
+    {
+      current: true,
+      label: 'Devices',
+      icon: (
+        <Chip
+          fill="white"
+          description="Icon"
+          className="bx--header__menu-item bx--header__menu-title"
+        />
+      ),
+      onClick: null,
+      href: 'javascript:void(0)',
+      linkContent: 'Devices',
+    },
+    {
+      label: 'Members',
+      icon: (
+        <Group
+          fill="white"
+          description="Icon"
+          className="bx--header__menu-item bx--header__menu-title"
+        />
+      ),
+      onClick: null,
+      href: 'javascript:void(0)',
+      linkContent: 'Members',
+      childContent: [
+        {
+          onClick: jest.fn(),
+          href: 'javascript:void(0)',
+          content: 'Yet another link',
+        },
+      ],
+    },
+  ];
+  /* eslint-enable */
+  let mockProps;
 
-    const header = shallow(
-      <Header
-        title="My Title"
-        user="j@test.com"
-        tenant="acme"
-        appName="platform"
-        actionItems={actionItems}
-      />
-    );
+  beforeEach(() => {
+    mockProps = {
+      links,
+      'aria-label': 'Side navigation',
+    };
+  });
 
-    expect(header).toMatchSnapshot();
+  it('should render an a tag', () => {
+    const wrapper = mount(<SideNav {...mockProps} />);
+    expect(wrapper).toMatchSnapshot();
+    expect(
+      render(
+        wrapper
+          .find('ul')
+          .first()
+          .childAt(0)
+      )
+        .children()
+        .first()[0].name
+    ).toEqual('a');
+  });
+
+  it('should render an button tag', () => {
+    const wrapper = mount(<SideNav {...mockProps} />);
+    expect(wrapper).toMatchSnapshot();
+    expect(
+      render(
+        wrapper
+          .find('ul')
+          .first()
+          .childAt(2)
+      )
+        .children()
+        .first()[0].name
+    ).toEqual('button');
+  });
+
+  it('should be collapsed by default', () => {
+    const wrapper = mount(<CarbonSideNav {...mockProps} />);
+    expect(wrapper.state('isExpanded')).toBe(false);
+  });
+
+  it('should be expanded by default', () => {
+    const wrapper = mount(<CarbonSideNav {...mockProps} defaultExpanded />);
+    expect(wrapper.state('isExpanded')).toBe(true);
   });
 });
