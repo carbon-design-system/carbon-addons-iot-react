e0335c17c0c3f6cab38ded4810616ec5734815c1
Scott Dickerson <swiftfoot@hotmail.com>
2019-04-08 16:48:56 -0500

    test(pagination): add testcases

--- src/components/SimplePagination/SimplePagination.test.jsx
+++ src/components/SimplePagination/SimplePagination.test.jsx
@@ -24,4 +24,30 @@ describe('SimplePagination', () => {
     // Because the page number is the same as maxPage we shouldn't have any clickable or tabbable buttons
     expect(tabbableButtons).toHaveLength(2);
   });
+  test('next and prev', () => {
+    const mockPage = jest.fn();
+    const wrapper = mount(<SimplePagination page={2} maxPage={4} onPage={mockPage} />);
+    const nextAndPrevButtons = wrapper.find('div[onClick]');
+
+    // Next button should be page 3
+    nextAndPrevButtons.at(1).simulate('click');
+    expect(mockPage).toHaveBeenCalledWith(3);
+
+    // Prev button should be page 1
+    nextAndPrevButtons.at(0).simulate('click');
+    expect(mockPage).toHaveBeenCalledWith(1);
+  });
+  test('next and prev key down', () => {
+    const mockPage = jest.fn();
+    const wrapper = mount(<SimplePagination page={2} maxPage={4} onPage={mockPage} />);
+    const nextAndPrevButtons = wrapper.find('div[tabIndex=0]');
+
+    // Next button should be page 3
+    nextAndPrevButtons.at(1).simulate('keydown', { key: 'Enter', keyCode: 13, which: 13 });
+    expect(mockPage).toHaveBeenCalledWith(3);
+
+    // Prev button should be page 1
+    nextAndPrevButtons.at(0).simulate('keydown', { key: 'Enter', keyCode: 13, which: 13 });
+    expect(mockPage).toHaveBeenCalledWith(1);
+  });
 });
