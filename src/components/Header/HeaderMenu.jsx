67a30fd8693305365f7215355a164fdab94724f9
Bryan Boyd <bryancboyd@gmail.com>
2019-05-14 16:57:39 -0500

    chore(clean): remove source headers

--- src/components/Header/HeaderMenu.jsx
+++ src/components/Header/HeaderMenu.jsx
@@ -1,10 +1,3 @@
-/**
- * Copyright IBM Corp. 2016, 2018
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import ChevronDownGlyph from '@carbon/icons-react/lib/chevron--down';
 import { settings } from 'carbon-components';
 import cx from 'classnames';
