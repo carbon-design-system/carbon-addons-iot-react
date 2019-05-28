c70ae382b26a795d37cffedc2c7ea8633a39fbaf
Bryan Boyd <bryancboyd@gmail.com>
2019-05-28 17:01:52 -0500

    test(dashboard): add story for 16 col dashboard

--- src/components/Dashboard/Dashboard.story.jsx
+++ src/components/Dashboard/Dashboard.story.jsx
@@ -6,6 +6,7 @@ import { Button } from 'carbon-components-react';
 
 import {
   DASHBOARD_BREAKPOINTS,
+  DASHBOARD_COLUMNS,
   CARD_DIMENSIONS,
   ROW_HEIGHT,
   CARD_SIZES,
@@ -105,6 +106,92 @@ const originalCards = [
   },
 ];
 
+export const DASHBOARD_SIZES_16_COL = {
+  MAX: 'max',
+  XLARGE: 'xl',
+  LARGE: 'lg',
+  MEDIUM: 'md',
+  SMALL: 'sm',
+  XSMALL: 'xs',
+};
+
+export const DASHBOARD_COLUMNS_16_COL = {
+  max: 16,
+  xl: 16,
+  lg: 16,
+  md: 8,
+  sm: 4,
+  xs: 4,
+};
+
+export const DASHBOARD_BREAKPOINTS_16_COL = {
+  max: 1800,
+  xl: 1312,
+  lg: 1056,
+  md: 672,
+  sm: 480,
+  xs: 320,
+};
+
+const CARD_DIMENSIONS_16_COL = {
+  XSMALL: {
+    max: { w: 2, h: 1 },
+    xl: { w: 2, h: 1 },
+    lg: { w: 4, h: 1 },
+    md: { w: 2, h: 1 },
+    sm: { w: 2, h: 1 },
+    xs: { w: 4, h: 1 },
+  },
+  SMALL: {
+    max: { w: 2, h: 2 },
+    xl: { w: 4, h: 2 },
+    lg: { w: 4, h: 2 },
+    md: { w: 4, h: 2 },
+    sm: { w: 2, h: 2 },
+    xs: { w: 4, h: 2 },
+  },
+  TALL: {
+    max: { w: 2, h: 4 },
+    xl: { w: 4, h: 4 },
+    lg: { w: 4, h: 4 },
+    md: { w: 4, h: 2 },
+    sm: { w: 2, h: 4 },
+    xs: { w: 4, h: 4 },
+  },
+  MEDIUM: {
+    max: { w: 6, h: 2 },
+    xl: { w: 8, h: 2 },
+    lg: { w: 8, h: 2 },
+    md: { w: 8, h: 2 },
+    sm: { w: 4, h: 2 },
+    xs: { w: 4, h: 2 },
+  },
+  WIDE: {
+    max: { w: 8, h: 2 },
+    xl: { w: 8, h: 2 },
+    lg: { w: 12, h: 2 },
+    md: { w: 8, h: 2 },
+    sm: { w: 4, h: 2 },
+    xs: { w: 4, h: 2 },
+  },
+  LARGE: {
+    max: { w: 6, h: 4 },
+    xl: { w: 8, h: 4 },
+    lg: { w: 8, h: 4 },
+    md: { w: 8, h: 4 },
+    sm: { w: 4, h: 4 },
+    xs: { w: 4, h: 4 },
+  },
+  XLARGE: {
+    max: { w: 8, h: 4 },
+    xl: { w: 12, h: 4 },
+    lg: { w: 16, h: 4 },
+    md: { w: 8, h: 4 },
+    sm: { w: 4, h: 4 },
+    xs: { w: 4, h: 4 },
+  },
+};
+
 const StatefulDashboard = ({ ...props }) => {
   const [cards, setCards] = useState(originalCards);
 
@@ -148,14 +235,28 @@ const StatefulDashboard = ({ ...props }) => {
   );
 };
 
-storiesOf('Dashboard (Experimental)', module).add('basic', () => {
-  return (
-    <StatefulDashboard
-      title={text('title', 'Munich Building')}
-      isEditable={boolean('isEditable', true)}
-      dashboardBreakpoints={object('breakpoints', DASHBOARD_BREAKPOINTS)}
-      cardDimensions={object('card dimensions', CARD_DIMENSIONS)}
-      rowHeight={object('row height', ROW_HEIGHT)}
-    />
-  );
-});
+storiesOf('Dashboard (Experimental)', module)
+  .add('basic - 12 col', () => {
+    return (
+      <StatefulDashboard
+        title={text('title', 'Munich Building')}
+        isEditable={boolean('isEditable', true)}
+        dashboardBreakpoints={object('breakpoints', DASHBOARD_BREAKPOINTS)}
+        dashboardColumns={object('columns', DASHBOARD_COLUMNS)}
+        cardDimensions={object('card dimensions', CARD_DIMENSIONS)}
+        rowHeight={object('row height', ROW_HEIGHT)}
+      />
+    );
+  })
+  .add('basic - 16 col', () => {
+    return (
+      <StatefulDashboard
+        title={text('title', 'Munich Building')}
+        isEditable={boolean('isEditable', true)}
+        dashboardBreakpoints={object('breakpoints', DASHBOARD_BREAKPOINTS_16_COL)}
+        dashboardColumns={object('columns', DASHBOARD_COLUMNS_16_COL)}
+        cardDimensions={object('card dimensions', CARD_DIMENSIONS_16_COL)}
+        rowHeight={object('row height', ROW_HEIGHT)}
+      />
+    );
+  });
