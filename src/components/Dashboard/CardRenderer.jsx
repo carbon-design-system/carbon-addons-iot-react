9197b916138a05213c100b16580f1f3f252d7756
Scott Dickerson <swiftfoot@hotmail.com>
2019-10-01 15:25:09 -0500

    fix(cardrenderer): cached card action wasn't firing

--- src/components/Dashboard/CardRenderer.jsx
+++ src/components/Dashboard/CardRenderer.jsx
@@ -1,6 +1,8 @@
-import React, { useMemo, useCallback } from 'react';
+import React, { useMemo } from 'react';
 import merge from 'lodash/merge';
 import isEmpty from 'lodash/isEmpty';
+import isEqual from 'lodash/isEqual';
+import pick from 'lodash/pick';
 
 import ValueCard from '../ValueCard/ValueCard';
 import ImageCard from '../ImageCard/ImageCard';
@@ -11,166 +13,173 @@ import PieCard from '../PieCard/PieCard';
 import TimeSeriesCard from '../TimeSeriesCard/TimeSeriesCard';
 import { CARD_TYPES } from '../../constants/LayoutConstants';
 
+const areCardsEqual = (prevProps, nextProps) => {
+  return isEqual(pick(prevProps, ['style', 'card']), pick(nextProps[('style', 'card')]));
+};
+
 /**
  * This component decides which card component to render when passed a certain card type
  * It also caches some properties between renders to speed performance
  */
-const CardRenderer = ({
-  style, // eslint-disable-line
-  card, // eslint-disable-line
-  card: { availableActions, type, dataSource, isExpanded, ...others }, // eslint-disable-line
-  onCardAction, // eslint-disable-line
-  i18n, // eslint-disable-line
-  dashboardBreakpoints, // eslint-disable-line
-  cardDimensions, // eslint-disable-line
-  dashboardColumns, // eslint-disable-line
-  rowHeight, // eslint-disable-line
-  isLoading, // eslint-disable-line
-  isEditable, // eslint-disable-line
-  breakpoint, // eslint-disable-line
-  ...gridProps
-}) => {
-  // Speed up performance by caching
-  const cachedActions = useMemo(
-    () =>
-      merge(availableActions, {
-        range: !isEmpty(dataSource), // all cards should have the range picker
-        expand:
-          type === CARD_TYPES.IMAGE || type === CARD_TYPES.TIMESERIES || type === CARD_TYPES.TABLE, // image and line chart cards should have expand
-      }),
-    [availableActions, dataSource, type]
-  );
+const CardRenderer = React.memo(
+  ({
+    style, // eslint-disable-line
+    card, // eslint-disable-line
+    card: { availableActions, type, dataSource, isExpanded, ...others }, // eslint-disable-line
+    onCardAction, // eslint-disable-line
+    i18n, // eslint-disable-line
+    dashboardBreakpoints, // eslint-disable-line
+    cardDimensions, // eslint-disable-line
+    dashboardColumns, // eslint-disable-line
+    rowHeight, // eslint-disable-line
+    isLoading, // eslint-disable-line
+    isEditable, // eslint-disable-line
+    breakpoint, // eslint-disable-line
+    ...gridProps
+  }) => {
+    // Speed up performance by caching
+    const cachedActions = useMemo(
+      () =>
+        merge(availableActions, {
+          range: !isEmpty(dataSource), // all cards should have the range picker
+          expand:
+            type === CARD_TYPES.IMAGE ||
+            type === CARD_TYPES.TIMESERIES ||
+            type === CARD_TYPES.TABLE, // image and line chart cards should have expand
+        }),
+      [availableActions, dataSource, type]
+    );
 
-  const cachedOnCardAction = useCallback(onCardAction, [card.id]);
+    const cachedExpandedStyle = useMemo(
+      () => (isExpanded ? merge({ height: '100%', width: '100%', padding: '50' }, style) : style),
+      [isExpanded, style]
+    );
 
-  const cachedExpandedStyle = useMemo(
-    () => (isExpanded ? merge({ height: '100%', width: '100%', padding: '50' }, style) : style),
-    [isExpanded, style]
-  );
-
-  return (
-    <div key={card.id} {...gridProps} style={cachedExpandedStyle}>
-      {type === CARD_TYPES.VALUE ? (
-        <ValueCard
-          {...others}
-          key={card.id}
-          availableActions={cachedActions}
-          dataSource={dataSource}
-          type={type}
-          i18n={i18n}
-          isLoading={card.isLoading || isLoading}
-          isEditable={isEditable}
-          onCardAction={cachedOnCardAction}
-          breakpoint={breakpoint}
-          dashboardBreakpoints={dashboardBreakpoints}
-          dashboardColumns={dashboardColumns}
-          cardDimensions={cardDimensions}
-          rowHeight={rowHeight}
-        />
-      ) : type === CARD_TYPES.IMAGE ? (
-        <ImageCard
-          {...others}
-          key={card.id}
-          availableActions={cachedActions}
-          dataSource={dataSource}
-          type={type}
-          i18n={i18n}
-          isLoading={card.isLoading || isLoading}
-          isEditable={isEditable}
-          onCardAction={cachedOnCardAction}
-          breakpoint={breakpoint}
-          dashboardBreakpoints={dashboardBreakpoints}
-          dashboardColumns={dashboardColumns}
-          cardDimensions={cardDimensions}
-          rowHeight={rowHeight}
-        />
-      ) : type === CARD_TYPES.TIMESERIES ? (
-        <TimeSeriesCard
-          {...others}
-          key={card.id}
-          availableActions={cachedActions}
-          dataSource={dataSource}
-          type={type}
-          i18n={i18n}
-          isLoading={card.isLoading || isLoading}
-          isEditable={isEditable}
-          onCardAction={cachedOnCardAction}
-          breakpoint={breakpoint}
-          dashboardBreakpoints={dashboardBreakpoints}
-          dashboardColumns={dashboardColumns}
-          cardDimensions={cardDimensions}
-          rowHeight={rowHeight}
-        />
-      ) : type === CARD_TYPES.TABLE ? (
-        <TableCard
-          {...others}
-          key={card.id}
-          availableActions={cachedActions}
-          dataSource={dataSource}
-          type={type}
-          i18n={i18n}
-          isLoading={card.isLoading || isLoading}
-          isEditable={isEditable}
-          onCardAction={cachedOnCardAction}
-          breakpoint={breakpoint}
-          dashboardBreakpoints={dashboardBreakpoints}
-          dashboardColumns={dashboardColumns}
-          cardDimensions={cardDimensions}
-          rowHeight={rowHeight}
-        />
-      ) : type === CARD_TYPES.DONUT ? (
-        <DonutCard
-          {...others}
-          key={card.id}
-          availableActions={cachedActions}
-          dataSource={dataSource}
-          type={type}
-          i18n={i18n}
-          isLoading={card.isLoading || isLoading}
-          isEditable={isEditable}
-          onCardAction={cachedOnCardAction}
-          breakpoint={breakpoint}
-          dashboardBreakpoints={dashboardBreakpoints}
-          dashboardColumns={dashboardColumns}
-          cardDimensions={cardDimensions}
-          rowHeight={rowHeight}
-        />
-      ) : type === CARD_TYPES.PIE ? (
-        <PieCard
-          {...others}
-          key={card.id}
-          availableActions={cachedActions}
-          dataSource={dataSource}
-          type={type}
-          i18n={i18n}
-          isLoading={card.isLoading || isLoading}
-          isEditable={isEditable}
-          onCardAction={cachedOnCardAction}
-          breakpoint={breakpoint}
-          dashboardBreakpoints={dashboardBreakpoints}
-          dashboardColumns={dashboardColumns}
-          cardDimensions={cardDimensions}
-          rowHeight={rowHeight}
-        />
-      ) : type === CARD_TYPES.BAR ? (
-        <BarChartCard
-          {...others}
-          key={card.id}
-          availableActions={cachedActions}
-          dataSource={dataSource}
-          type={type}
-          i18n={i18n}
-          isLoading={card.isLoading || isLoading}
-          isEditable={isEditable}
-          onCardAction={cachedOnCardAction}
-          breakpoint={breakpoint}
-          dashboardBreakpoints={dashboardBreakpoints}
-          dashboardColumns={dashboardColumns}
-          cardDimensions={cardDimensions}
-          rowHeight={rowHeight}
-        />
-      ) : null}
-    </div>
-  );
-};
-export default React.memo(CardRenderer);
+    return (
+      <div key={card.id} {...gridProps} style={cachedExpandedStyle}>
+        {type === CARD_TYPES.VALUE ? (
+          <ValueCard
+            {...others}
+            key={card.id}
+            availableActions={cachedActions}
+            dataSource={dataSource}
+            type={type}
+            i18n={i18n}
+            isLoading={card.isLoading || isLoading}
+            isEditable={isEditable}
+            onCardAction={onCardAction}
+            breakpoint={breakpoint}
+            dashboardBreakpoints={dashboardBreakpoints}
+            dashboardColumns={dashboardColumns}
+            cardDimensions={cardDimensions}
+            rowHeight={rowHeight}
+          />
+        ) : type === CARD_TYPES.IMAGE ? (
+          <ImageCard
+            {...others}
+            key={card.id}
+            availableActions={cachedActions}
+            dataSource={dataSource}
+            type={type}
+            i18n={i18n}
+            isLoading={card.isLoading || isLoading}
+            isEditable={isEditable}
+            onCardAction={onCardAction}
+            breakpoint={breakpoint}
+            dashboardBreakpoints={dashboardBreakpoints}
+            dashboardColumns={dashboardColumns}
+            cardDimensions={cardDimensions}
+            rowHeight={rowHeight}
+          />
+        ) : type === CARD_TYPES.TIMESERIES ? (
+          <TimeSeriesCard
+            {...others}
+            key={card.id}
+            availableActions={cachedActions}
+            dataSource={dataSource}
+            type={type}
+            i18n={i18n}
+            isLoading={card.isLoading || isLoading}
+            isEditable={isEditable}
+            onCardAction={onCardAction}
+            breakpoint={breakpoint}
+            dashboardBreakpoints={dashboardBreakpoints}
+            dashboardColumns={dashboardColumns}
+            cardDimensions={cardDimensions}
+            rowHeight={rowHeight}
+          />
+        ) : type === CARD_TYPES.TABLE ? (
+          <TableCard
+            {...others}
+            key={card.id}
+            availableActions={cachedActions}
+            dataSource={dataSource}
+            type={type}
+            i18n={i18n}
+            isLoading={card.isLoading || isLoading}
+            isEditable={isEditable}
+            onCardAction={onCardAction}
+            breakpoint={breakpoint}
+            dashboardBreakpoints={dashboardBreakpoints}
+            dashboardColumns={dashboardColumns}
+            cardDimensions={cardDimensions}
+            rowHeight={rowHeight}
+          />
+        ) : type === CARD_TYPES.DONUT ? (
+          <DonutCard
+            {...others}
+            key={card.id}
+            availableActions={cachedActions}
+            dataSource={dataSource}
+            type={type}
+            i18n={i18n}
+            isLoading={card.isLoading || isLoading}
+            isEditable={isEditable}
+            onCardAction={onCardAction}
+            breakpoint={breakpoint}
+            dashboardBreakpoints={dashboardBreakpoints}
+            dashboardColumns={dashboardColumns}
+            cardDimensions={cardDimensions}
+            rowHeight={rowHeight}
+          />
+        ) : type === CARD_TYPES.PIE ? (
+          <PieCard
+            {...others}
+            key={card.id}
+            availableActions={cachedActions}
+            dataSource={dataSource}
+            type={type}
+            i18n={i18n}
+            isLoading={card.isLoading || isLoading}
+            isEditable={isEditable}
+            onCardAction={onCardAction}
+            breakpoint={breakpoint}
+            dashboardBreakpoints={dashboardBreakpoints}
+            dashboardColumns={dashboardColumns}
+            cardDimensions={cardDimensions}
+            rowHeight={rowHeight}
+          />
+        ) : type === CARD_TYPES.BAR ? (
+          <BarChartCard
+            {...others}
+            key={card.id}
+            availableActions={cachedActions}
+            dataSource={dataSource}
+            type={type}
+            i18n={i18n}
+            isLoading={card.isLoading || isLoading}
+            isEditable={isEditable}
+            onCardAction={onCardAction}
+            breakpoint={breakpoint}
+            dashboardBreakpoints={dashboardBreakpoints}
+            dashboardColumns={dashboardColumns}
+            cardDimensions={cardDimensions}
+            rowHeight={rowHeight}
+          />
+        ) : null}
+      </div>
+    );
+  },
+  areCardsEqual
+);
+export default CardRenderer;
