038bf966e5fdc128557cefe3942a67354d25ab7c
Scott Dickerson <swiftfoot@hotmail.com>
2019-10-17 16:14:04 -0500

    fix(babel): fix build failure with missing core-js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function generateConfig(api) {
   api.cache(true);
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+    presets: [
+      ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 2 }],
+      '@babel/preset-react',
+      '@babel/preset-flow',
+    ],
     ignore: ['__mocks__'],
     plugins: [
       'babel-plugin-lodash',
