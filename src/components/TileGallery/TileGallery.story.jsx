61e8ce6e0aea8fe90d13dd9ceabf54454780b63c
Bryan Boyd <bryancboyd@gmail.com>
2019-11-07 17:03:19 -0600

    feat(tilegallery): initial (experimental) drop

---
+++ src/components/TileGallery/TileGallery.story.jsx
@@ -0,0 +1,40 @@
+/* Used dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { PageTitleBar, Link } from '../../index';
+
+import { TileGallery, TileGallerySection, TileGalleryItem } from './TileGallery';
+
+export const content = (
+  <TileGallery>
+    <TileGallerySection title="Section 1">
+      <TileGalleryItem title="00001" description="The first one" isFavorite />
+      <TileGalleryItem title="00002" description="The second one" />
+      <TileGalleryItem title="00005" description="The fifth one" isFavorite />
+      <TileGalleryItem title="00003" />
+    </TileGallerySection>
+    <TileGallerySection title="More">
+      <TileGalleryItem title="00010" description="Another one" />
+    </TileGallerySection>
+  </TileGallery>
+);
+
+storiesOf('Watson IoT Experimental|TileGallery', module)
+  .add('basic example', () => (
+    <div style={{ position: 'fixed', left: 0, right: 0, top: 0, bottom: 0 }}>{content}</div>
+  ))
+  .add('wrapped in PageTitleBar', () => (
+    <div style={{ position: 'fixed', left: 0, right: 0, top: 0, bottom: 0 }}>
+      <PageTitleBar
+        title="A cool PageWizard!"
+        description="The description from the PageTitleBar"
+        breadcrumb={[
+          <Link to="www.ibm.com">Home</Link>,
+          <Link to="www.ibm.com">Something</Link>,
+          <Link to="www.ibm.com">Something Else</Link>,
+        ]}
+        content={content}
+      />
+    </div>
+  ));
---
+++ src/components/TileGallery/TileGallery.test.jsx
@@ -0,0 +1,10 @@
+import { shallow } from 'enzyme';
+
+import { content } from './TileGallery.story';
+
+describe('TileGallery tests', () => {
+  test('currentStepId prop', () => {
+    const wrapper = shallow(content);
+    expect(wrapper.find('TileGallerySection').prop('title')).toEqual('Section 1');
+  });
+});
