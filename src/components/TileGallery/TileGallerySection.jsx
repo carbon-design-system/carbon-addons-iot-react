6ed4fb863c5e7d193678ed929bd0dc46f0738dc5
Marcelo Blechner <marcblec@br.ibm.com>
2019-11-08 18:46:45 -0300

    Added a `hasAccordion` property (default true)

--- src/components/TileGallery/TileGallerySection.jsx
+++ src/components/TileGallery/TileGallerySection.jsx
@@ -3,26 +3,34 @@ import PropTypes from 'prop-types';
 
 import { Accordion, AccordionItem } from '../../index';
 
-const TileGallerySection = ({ children, title, isOpen = true, onClick = () => {}, ...rest }) => {
+const TileGallerySection = ({ children, title, isOpen = true, hasAccordion = true, onClick = () => {}, ...rest }) => {
   const [open, setOpen] = useState(isOpen);
   return (
     <div className="tile-gallery--section">
-      <Accordion>
-        <AccordionItem
-          title={title}
-          onHeadingClick={evt => {
-            setOpen(!open);
-            onClick(evt);
-          }}
-          open={open}
-        >
-          <div className="tile-gallery--section--items">
-            {React.Children.map(children, tileGalleryItem =>
-              React.cloneElement(tileGalleryItem, { mode: rest.mode })
-            )}
-          </div>
-        </AccordionItem>
-      </Accordion>
+      {hasAccordion ? (
+        <Accordion>
+          <AccordionItem
+            title={title}
+            onHeadingClick={evt => {
+              setOpen(!open);
+              onClick(evt);
+            }}
+            open={open}
+          >
+            <div className="tile-gallery--section--items">
+              {React.Children.map(children, tileGalleryItem =>
+                React.cloneElement(tileGalleryItem, { mode: rest.mode })
+              )}
+            </div>
+          </AccordionItem>
+        </Accordion>
+      ) : (
+        <div className="tile-gallery--section--items">
+          {React.Children.map(children, tileGalleryItem =>
+            React.cloneElement(tileGalleryItem, { mode: rest.mode })
+          )}
+        </div>
+      )}
     </div>
   );
 };
