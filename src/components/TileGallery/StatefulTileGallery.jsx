1e8ccbaf59b6a878a5946b46b892359fc85922ea
Claudia Tejos <cgirani@br.ibm.com>
2019-11-25 09:55:35 -0300

    chore(Stateful): remove not used imports

--- src/components/TileGallery/StatefulTileGallery.jsx
+++ src/components/TileGallery/StatefulTileGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
 import escapeRegExp from 'lodash/escapeRegExp';
