80a1c0438c9b5e0a7d97b976c204992e46a6f14a
lijianig <lijianig@cn.ibm.com>
2019-12-16 10:03:15 +0800

    fix(Table): remove preData in AsyncTable
    #720

--- src/components/Table/AsyncTable/AsyncTable.jsx
+++ src/components/Table/AsyncTable/AsyncTable.jsx
@@ -14,7 +14,6 @@ const AsyncTable = ({ fetchData, id }) => {
 
   const [state, dispatch] = useReducer(reducer, {
     data: [],
-    preData: [],
     view: {
       filters: [],
       pagination: {
