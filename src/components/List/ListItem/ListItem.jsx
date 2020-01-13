ce97d877a5a5d8a9d087d1685f588a10f3b52d24
Denny Nguyen <dlnguyen@us.ibm.com>
2020-01-13 14:12:59 -0600

    skeleton for listitem story

--- src/components/List/ListItem/ListItem.jsx
+++ src/components/List/ListItem/ListItem.jsx
@@ -12,6 +12,7 @@ const ListItem = ({
   isExpandable,
   isSelectable,
   nestingLevel,
+  content,
   ...others
 }) => {
   return (
@@ -36,7 +37,7 @@ const ListItem = ({
           </div>
         )}
 
-        <div className="list-item--content">{others.content}</div>
+        <div className="list-item--content">{content}</div>
       </div>
     </div>
   );
