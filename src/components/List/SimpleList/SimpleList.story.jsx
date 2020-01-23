5fa4149a009db0549c037117861b4db78bdcb701
Jessie <xyan@us.ibm.com>
2020-01-23 15:09:24 -0600

    fix(list): story for list with empty row

--- src/components/List/SimpleList/SimpleList.story.jsx
+++ src/components/List/SimpleList/SimpleList.story.jsx
@@ -19,6 +19,8 @@ const getListItems = num =>
       content: { value: `Item ${idx + 1}` },
     }));
 
+const listItemsWithEmptyRow = getListItems(5).concat({ id: 6, content: { value: '' } });
+
 const rowActions = [
   <Edit16 onClick={action('edit')} />,
   <Add16 onClick={action('add')} />,
@@ -194,6 +196,23 @@ storiesOf('Watson IoT Experimental|SimpleList', module)
           buttons={buttonsToRender}
           items={getListItems(20)}
           pageSize="sm"
+        />
+      </div>
+    )),
+    { info: { text: `` } }
+  )
+  .add(
+    'list with empty row',
+    withReadme(SimpleListREADME, () => (
+      <div style={{ width: 500, height: 500, background: '#fee', padding: 10 }}>
+        <SimpleList
+          title={text('Text', 'Simple List')}
+          hasSearch
+          i18n={{
+            searchPlaceHolderText: 'Enter a search',
+          }}
+          buttons={buttonsToRender}
+          items={listItemsWithEmptyRow}
         />
       </div>
     )),
