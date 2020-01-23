6741e18f0ac01ff0bb5b085db78f27eda6f7bfdf
Jessie <xyan@us.ibm.com>
2020-01-23 14:01:17 -0600

    feat(list): remove console log

--- src/components/List/ListItem/ListItem.jsx
+++ src/components/List/ListItem/ListItem.jsx
@@ -51,77 +51,74 @@ const ListItem = ({
   nestingLevel,
   isCategory,
   ...others
-}) => {
-  console.log(rowActions);
-  return (
+}) => (
+  <div
+    className={classnames('list-item', {
+      'list-item__selected': selected,
+      'list-item__large': isLargeRow,
+    })}
+    style={{ paddingLeft: ` ${nestingLevel * 30}px` }}
+  >
+    {isExpandable && (
+      <div
+        className="list-item--expand-icon"
+        onClick={() => {
+          if (isSelectable) {
+            onSelect(id);
+          }
+          if (isExpandable) {
+            onExpand(id);
+          }
+        }}
+      >
+        <Icon icon={expanded ? iconChevronUp : iconChevronDown} />
+      </div>
+    )}
+
     <div
-      className={classnames('list-item', {
-        'list-item__selected': selected,
-        'list-item__large': isLargeRow,
+      className={classnames('list-item--content', {
+        'list-item--content__selected': selected,
+        'list-item--content__large': isLargeRow,
       })}
-      style={{ paddingLeft: ` ${nestingLevel * 30}px` }}
     >
-      {isExpandable && (
-        <div
-          className="list-item--expand-icon"
-          onClick={() => {
-            if (isSelectable) {
-              onSelect(id);
-            }
-            if (isExpandable) {
-              onExpand(id);
-            }
-          }}
-        >
-          <Icon icon={expanded ? iconChevronUp : iconChevronDown} />
+      {icon && (
+        <div className={`list-item--content--icon list-item--content--icon__${iconPosition}`}>
+          {icon}
         </div>
       )}
-
       <div
-        className={classnames('list-item--content', {
-          'list-item--content__selected': selected,
-          'list-item--content__large': isLargeRow,
+        className={classnames('list-item--content--values', {
+          'list-item--content--values__large': isLargeRow,
         })}
       >
-        {icon && (
-          <div className={`list-item--content--icon list-item--content--icon__${iconPosition}`}>
-            {icon}
-          </div>
+        {isLargeRow ? (
+          <React.Fragment>
+            <div
+              className={classnames(
+                'list-item--content--values--main',
+                'list-item--content--values--main__large'
+              )}
+            >
+              <div title={value}>{value}</div>
+              {rowActions ? <div>{rowActions}</div> : null}
+            </div>
+            <div title={secondaryValue} className="list-item--content--values--secondary">
+              {secondaryValue || null}
+            </div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div className="list-item--content--values--main">
+              <div title={value}>{value}</div>
+              {secondaryValue ? <div title={secondaryValue}>{secondaryValue}</div> : null}
+              {rowActions ? <div>{rowActions}</div> : null}
+            </div>
+          </React.Fragment>
         )}
-        <div
-          className={classnames('list-item--content--values', {
-            'list-item--content--values__large': isLargeRow,
-          })}
-        >
-          {isLargeRow ? (
-            <React.Fragment>
-              <div
-                className={classnames(
-                  'list-item--content--values--main',
-                  'list-item--content--values--main__large'
-                )}
-              >
-                <div title={value}>{value}</div>
-                {rowActions ? <div>{rowActions}</div> : null}
-              </div>
-              <div title={secondaryValue} className="list-item--content--values--secondary">
-                {secondaryValue || null}
-              </div>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <div className="list-item--content--values--main">
-                <div title={value}>{value}</div>
-                {secondaryValue ? <div title={secondaryValue}>{secondaryValue}</div> : null}
-                {rowActions ? <div>{rowActions}</div> : null}
-              </div>
-            </React.Fragment>
-          )}
-        </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 ListItem.propTypes = propTypes;
 ListItem.defaultProps = defaultProps;
