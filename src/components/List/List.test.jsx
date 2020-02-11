6bd0ee6b9f84c88c4de26fca7ecfb1c3e7eafc67
Jessie <xyan@us.ibm.com>
2020-02-11 11:24:44 -0600

    fix(list): improve test coverage for list

--- src/components/List/List.test.jsx
+++ src/components/List/List.test.jsx
@@ -5,18 +5,75 @@ import List from './List';
 import { sampleHierarchy } from './List.story';
 
 describe('List component tests', () => {
-  test('list gets rendered', () => {
+  const getListItems = num =>
+    Array(num)
+      .fill(0)
+      .map((i, idx) => ({
+        id: idx + 1,
+        content: { value: `Item ${idx + 1}` },
+        isSelectable: true,
+      }));
+
+  test('List when pagination is null', () => {
+    const renderedElement = render(<List title="list" items={getListItems(5)} />);
+    expect(renderedElement.container.innerHTML).toBeTruthy();
+  });
+
+  test('List to have default handleSelect', () => {
+    expect(List.defaultProps.handleSelect).toBeDefined();
+    List.defaultProps.handleSelect();
+  });
+
+  test('List to have default toggleExpansion', () => {
+    expect(List.defaultProps.toggleExpansion).toBeDefined();
+    List.defaultProps.toggleExpansion();
+  });
+
+  test('List when selectedIds is set', () => {
     const renderedElement = render(
-      <List
-        title="list"
-        items={Object.entries(sampleHierarchy.MLB['American League']['New York Yankees']).map(
-          ([key]) => ({
-            id: key,
-            content: { value: key },
-          })
-        )}
-      />
+      <List title="list" items={getListItems(5)} selectedIds={[1, 2]} />
     );
     expect(renderedElement.container.innerHTML).toBeTruthy();
+  });
+
+  test('List hasChildren and expanded', () => {
+    const { getByTitle } = render(
+      <List
+        title="list"
+        items={[
+          ...Object.keys(sampleHierarchy.MLB['American League']).map(team => ({
+            id: team,
+            isCategory: true,
+            content: {
+              value: team,
+            },
+            children: Object.keys(sampleHierarchy.MLB['American League'][team]).map(player => ({
+              id: `${team}_${player}`,
+              content: {
+                value: player,
+                secondaryValue: sampleHierarchy.MLB['American League'][team][player],
+              },
+              isSelectable: true,
+            })),
+          })),
+          ...Object.keys(sampleHierarchy.MLB['National League']).map(team => ({
+            id: team,
+            isCategory: true,
+            content: {
+              value: team,
+            },
+            children: Object.keys(sampleHierarchy.MLB['National League'][team]).map(player => ({
+              id: `${team}_${player}`,
+              content: {
+                value: player,
+                secondaryValue: sampleHierarchy.MLB['National League'][team][player],
+              },
+              isSelectable: true,
+            })),
+          })),
+        ]}
+      />
+    );
+    expect(getByTitle('Chicago White Sox')).toBeTruthy();
   });
 });
--- src/components/List/ListItem/ListItem.test.jsx
+++ src/components/List/ListItem/ListItem.test.jsx
@@ -1,11 +1,72 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, getByText } from '@testing-library/react';
+import { Add16, Edit16 } from '@carbon/icons-react';
 
 import ListItem from './ListItem';
 
-describe('SimpleList component tests', () => {
-  test('ListItem gets rendered', () => {
-    const renderedElement = render(<ListItem id="1" value="" />);
-    expect(renderedElement.container.innerHTML).toBeTruthy();
+describe('ListItem component tests', () => {
+  test('test ListItem gets rendered', () => {
+    const { getByText } = render(<ListItem id="1" value="test" />);
+    expect(getByText('test')).toBeTruthy();
+  });
+
+  test('ListItem with large row and secondary value', () => {
+    const { getByText } = render(
+      <ListItem id="1" value="test" secondaryValue="second" isLargeRow />
+    );
+    expect(getByText('test')).toBeTruthy();
+    expect(getByText('second')).toBeTruthy();
+  });
+
+  test('ListItem when isSelectable set to true', () => {
+    const onSelect = jest.fn();
+    const { getAllByRole } = render(
+      <ListItem id="1" value="test" isSelectable onSelect={onSelect} />
+    );
+    fireEvent.keyPress(getAllByRole('button')[0], { key: 'Enter', charCode: 13 });
+    expect(onSelect).toHaveBeenCalledTimes(1);
+  });
+
+  test('ListItem when isSelectable is set to true and onClick will trigger onSelect', () => {
+    const onSelect = jest.fn();
+    const { getAllByRole } = render(<ListItem id="1" value="" isSelectable onSelect={onSelect} />);
+    fireEvent.click(getAllByRole('button')[0]);
+    expect(onSelect).toHaveBeenCalledTimes(1);
+  });
+
+  test('ListItem when is Expandable set to true', () => {
+    const onExpand = jest.fn();
+    const { getAllByRole } = render(<ListItem id="1" value="" isExpandable onExpand={onExpand} />);
+    fireEvent.keyPress(getAllByRole('button')[0], { key: 'Enter', charCode: 13 });
+    expect(onExpand).toHaveBeenCalledTimes(1);
+  });
+
+  test('ListItem when is Expandable set to true and onClick will trigger onExpand', () => {
+    const onExpand = jest.fn();
+    const { getAllByRole } = render(<ListItem id="1" value="" isExpandable onExpand={onExpand} />);
+    fireEvent.click(getAllByRole('button')[0]);
+    expect(onExpand).toHaveBeenCalledTimes(1);
+  });
+
+  test('ListItem with Icon', () => {
+    const onClick = jest.fn();
+    const { getByTitle } = render(
+      <ListItem
+        id="1"
+        value="test"
+        icon={<Add16 title="iconTitle" onClick={onClick} />}
+        iconPosition="left"
+      />
+    );
+    fireEvent.click(getByTitle('iconTitle'));
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('ListItem with rowActions', () => {
+    const rowActionOnClick = jest.fn();
+    const rowActions = [<Edit16 title="iconTitle" onClick={rowActionOnClick} />];
+    const { getByTitle } = render(<ListItem id="1" value="test" rowActions={rowActions} />);
+    fireEvent.click(getByTitle('iconTitle'));
+    expect(rowActionOnClick).toHaveBeenCalledTimes(1);
   });
 });
