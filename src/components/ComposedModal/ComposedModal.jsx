fcd2d8f9a856cc01445768ea3751d8db7e0be6ec
Scott Dickerson <swiftfoot@hotmail.com>
2020-02-18 15:27:19 -0600

    docs(composedmodal): add missing prop-type

--- src/components/ComposedModal/ComposedModal.jsx
+++ src/components/ComposedModal/ComposedModal.jsx
@@ -106,6 +106,8 @@ export const ComposedModalPropTypes = {
       secondaryButtonLabel: PropTypes.node,
       /** should the primary button be hidden (i.e. only show Cancel) */
       isPrimaryButtonHidden: PropTypes.bool,
+      /** should the primary button be disabled */
+      isPrimaryButtonDisabled: PropTypes.bool,
     }),
   ]),
   /** NEW PROP: Type of dialog, affects colors, styles of dialog */
