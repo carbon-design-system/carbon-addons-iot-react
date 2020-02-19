2d41f7817a8abbef29cf24823f1b9e9cb85c4bc9
Taylor Jones <taylor.jones826@gmail.com>
2020-02-19 12:47:45 -0600

    test(rowactionscell): update snapshots

--- src/components/Table/TableBody/RowActionsCell/RowActionsCell.jsx
+++ src/components/Table/TableBody/RowActionsCell/RowActionsCell.jsx
@@ -186,7 +186,7 @@ class RowActionsCell extends React.Component {
                     {...others}
                     iconDescription={overflowMenuAria}
                     key={`${tableId}-${id}-row-actions-button-${actionId}`}
-                    id={`${tableId}-${id}-row-actions-button-${actionId}`}
+                    data-testid={`${tableId}-${id}-row-actions-button-${actionId}`}
                     kind="ghost"
                     onClick={e => onClick(e, id, actionId, onApplyRowAction)}
                   >
@@ -196,6 +196,7 @@ class RowActionsCell extends React.Component {
               {hasOverflow ? (
                 <StyledOverflowMenu
                   id={`${tableId}-${id}-row-actions-cell-overflow`}
+                  data-testid={`${tableId}-${id}-row-actions-cell-overflow`}
                   flipped
                   ariaLabel={overflowMenuAria}
                   onClick={event => event.stopPropagation()}
