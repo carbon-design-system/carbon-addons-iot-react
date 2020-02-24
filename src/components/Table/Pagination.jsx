d529b9207042e3761357a2a24abc0f80ca05114f
Scott Dickerson <swiftfoot@hotmail.com>
2020-02-24 14:26:53 -0600

    test(pagination): add testcase to cover narrow width

--- src/components/Table/Pagination.jsx
+++ src/components/Table/Pagination.jsx
@@ -1,5 +1,6 @@
 import sizeMe from 'react-sizeme';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Pagination } from 'carbon-components-react';
 import classnames from 'classnames';
 
@@ -7,6 +8,10 @@ import { settings } from '../../constants/Settings';
 
 const { iotPrefix } = settings;
 
+/**
+ * This pagination component hides the items per page selection dropdown if the isItemsPerPageHidden bit is true.
+ * It also hides the Items per page and x of x items text if the total width of the pagination bar is less than 500 px
+ */
 const SizedPagination = sizeMe({ noPlaceholder: true })(
   ({ isItemPerPageHidden, size, className, ...rest }) => (
     <Pagination
@@ -18,5 +23,12 @@ const SizedPagination = sizeMe({ noPlaceholder: true })(
     />
   )
 );
+
+SizedPagination.propTypes = {
+  isItemPerPageHidden: PropTypes.bool,
+};
+SizedPagination.defaultProps = {
+  isItemPerPageHidden: false,
+};
 
 export default SizedPagination;
