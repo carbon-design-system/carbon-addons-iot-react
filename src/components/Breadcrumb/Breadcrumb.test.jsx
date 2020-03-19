33b1f240353f3d8781cbf938a8a3317b29e4006a
Taylor Jones <taylor.jones826@gmail.com>
2020-03-19 16:30:28 -0500

    test(breadcrumb): cover ResizeObserver console warning branch

--- src/components/Breadcrumb/Breadcrumb.test.jsx
+++ src/components/Breadcrumb/Breadcrumb.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BreadcrumbItem } from 'carbon-components-react';
 import { render } from '@testing-library/react';
+import useResizeObserver from 'use-resize-observer';
 
 import Breadcrumb from './Breadcrumb';
 
@@ -12,7 +13,7 @@ const originalOffsetHeight = Object.getOwnPropertyDescriptor(HTMLElement.prototy
 const originalOffsetWidth = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'scrollWidth');
 
 describe('Breadcrumb', () => {
-  test('overlows when container is smaller than breadcrumbs', () => {
+  test('overflows when container is smaller than breadcrumbs', () => {
     const { container } = render(
       <Breadcrumb {...commonProps} hasOverflow>
         <BreadcrumbItem href="#">Breadcrumb 1</BreadcrumbItem>
@@ -43,7 +44,7 @@ describe('Breadcrumb with overflow', () => {
     Object.defineProperty(HTMLElement.prototype, 'scrollWidth', originalOffsetWidth);
   });
 
-  test('overlows when container is smaller than breadcrumbs', () => {
+  test('overflows when container is smaller than breadcrumbs', () => {
     const { container } = render(
       <Breadcrumb {...commonProps} hasOverflow>
         <BreadcrumbItem href="#">Breadcrumb 1</BreadcrumbItem>
@@ -52,5 +53,36 @@ describe('Breadcrumb with overflow', () => {
       </Breadcrumb>
     );
     expect(container.querySelector('.breadcrumb--overflow')).toBeTruthy();
+  });
+
+  describe('has dev console warning(s)', () => {
+    const originalConsole = console.error;
+    const originalDev = window.__DEV__;
+    const originalResizeObserver = window.ResizeObserver;
+
+    // Applies only to tests in this describe block
+    beforeEach(() => {
+      window.__DEV__ = true;
+      window.ResizeObserver = undefined;
+      console.error = jest.fn();
+    });
+
+    test('when ResizeObserver is not supported in the current environment', () => {
+      render(
+        <Breadcrumb {...commonProps} hasOverflow>
+          <BreadcrumbItem href="#">Breadcrumb 1</BreadcrumbItem>
+          <BreadcrumbItem href="#">Breadcrumb 2</BreadcrumbItem>
+          <BreadcrumbItem href="#">Breadcrumb 3</BreadcrumbItem>
+        </Breadcrumb>
+      );
+      expect(console.error).toHaveBeenCalledTimes(1);
+    });
+
+    afterEach(() => {
+      // restore to original values
+      window.__DEV__ = originalDev;
+      window.ResizeObserver = originalResizeObserver;
+      console.error = originalConsole;
+    });
   });
 });
