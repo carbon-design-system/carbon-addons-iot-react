690cfcf11517e9b6804f96ebc8fbd63b0df2e52f
Scott Dickerson <swiftfoot@hotmail.com>
2020-05-19 13:14:29 -0500

    test(emptytable): add additional click tests

--- src/components/Table/EmptyTable/__tests__/EmptyTable.test.jsx
+++ src/components/Table/EmptyTable/__tests__/EmptyTable.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import EmptyTable from '../EmptyTable';
 
@@ -11,37 +11,40 @@ const commonTableProps = {
 
 describe('EmptyTable', () => {
   it('custom empty state', () => {
-    const { queryAllByText } = render(
-      <EmptyTable {...commonTableProps} emptyState={<span>my custom element</span>} />
-    );
-    expect(queryAllByText('my custom element')).toHaveLength(1);
+    render(<EmptyTable {...commonTableProps} emptyState={<span>my custom element</span>} />);
+    expect(screen.queryAllByText('my custom element')).toHaveLength(1);
   });
   it('no empty state action does not render button', () => {
-    const { queryAllByText } = render(
+    render(
       <EmptyTable
         {...commonTableProps}
         emptyState={{ message: 'I am empty', buttonLabel: 'clickme' }}
       />
     );
-    expect(queryAllByText('I am empty')).toHaveLength(1);
-    expect(queryAllByText('clickme')).toHaveLength(0);
+    expect(screen.queryAllByText('I am empty')).toHaveLength(1);
+    expect(screen.queryAllByText('clickme')).toHaveLength(0);
   });
   it('empty state action with custom button', () => {
-    const { queryAllByText } = render(
+    const mockEmptyStateAction = jest.fn();
+    render(
       <EmptyTable
         {...commonTableProps}
         emptyState={{
           message: 'I am empty',
           buttonLabel: 'clickme',
         }}
-        onEmptyStateAction={jest.fn()}
+        onEmptyStateAction={mockEmptyStateAction}
       />
     );
-    expect(queryAllByText('I am empty')).toHaveLength(1);
-    expect(queryAllByText('clickme')).toHaveLength(1);
+
+    expect(screen.queryAllByText('I am empty')).toHaveLength(1);
+    expect(screen.queryAllByText('clickme')).toHaveLength(1);
+    fireEvent.click(screen.getByText('clickme'));
+    expect(mockEmptyStateAction).toHaveBeenCalled();
   });
-  it('empty state action with custom filtered button', () => {
-    const { queryAllByText } = render(
+  it('empty state action with custom filtered button calls onEmptyStateAction', () => {
+    const mockEmptyStateAction = jest.fn();
+    render(
       <EmptyTable
         {...commonTableProps}
         isFiltered
@@ -49,10 +52,12 @@ describe('EmptyTable', () => {
           message: 'I am empty',
           buttonLabelWithFilters: 'clickmyfilters',
         }}
-        onEmptyStateAction={jest.fn()}
+        onEmptyStateAction={mockEmptyStateAction}
       />
     );
-    expect(queryAllByText('I am empty')).toHaveLength(1);
-    expect(queryAllByText('clickmyfilters')).toHaveLength(1);
+    expect(screen.queryAllByText('I am empty')).toHaveLength(1);
+    expect(screen.queryAllByText('clickmyfilters')).toHaveLength(1);
+    fireEvent.click(screen.getByText('clickmyfilters'));
+    expect(mockEmptyStateAction).toHaveBeenCalled();
   });
 });
