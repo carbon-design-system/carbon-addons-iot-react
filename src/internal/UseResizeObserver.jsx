745f170b51c829c2d57ecc790c8a78a2660a8e74
marcJV <marc.vandehey@gmail.com>
2020-06-16 10:55:23 -0500

    fix(deps): updated use-resize-observer dependency

---
+++ src/internal/UseResizeObserver.jsx
@@ -0,0 +1,23 @@
+import useResizeObserver from 'use-resize-observer';
+import warning from 'warning';
+
+import { browserSupports } from '../utils/componentUtilityFunctions';
+
+export const useResize = resizeRef => {
+  if (!browserSupports('ResizeObserver')) {
+    warning(
+      !__DEV__,
+      'The current browser does not support ResizeObserver. You will need to include a ResizeObserver polyfill for this component to function properly.'
+    );
+    return null;
+  }
+
+  /**
+   * use-resize-observer currently crashes if it is unsupported
+   * we need to conditionally set this hook for now until it is fixed in the library
+   */
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useResizeObserver(resizeRef);
+
+  return resizeRef;
+};
