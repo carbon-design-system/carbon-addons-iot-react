2cc73d2f0b98d96379e1baaae58ff71c49c9db53
David Conner <rdconner@us.ibm.com>
2020-06-29 12:28:30 -0400

    feat(combobox): change editOptionText default

--- src/components/ComboBox/ComboBox.jsx
+++ src/components/ComboBox/ComboBox.jsx
@@ -31,7 +31,7 @@ const defaultProps = {
   inline: false,
   wrapperClassName: null,
   closeButtonText: 'Close',
-  editOptionText: '+ Add',
+  editOptionText: '-Create',
   hasMultiValue: false,
   items: [],
 };
