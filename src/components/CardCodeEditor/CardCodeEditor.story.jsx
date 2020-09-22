287145f8d206fd4fffa705a5580c6f19e7ed3970
David Conner <rdconner@us.ibm.com>
2020-09-22 17:28:18 -0400

    feat(cardeditor): initial pass at card code editor

---
+++ src/components/CardCodeEditor/CardCodeEditor.story.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import CardCodeEditor from './CardCodeEditor';
+
+const pkgjson = require('../../../package.json');
+
+const isValidCallback = (val, setError) => {
+  try {
+    setError(false);
+    // comments are not valid JSON so remove before check
+    const _value = val.replace('// write your code here', '');
+
+    if (_value === '') {
+      setError('JSON value must not be an empty string');
+    } else {
+      const json = JSON.parse(_value);
+      // Check for non-exception throwing cases (false, 1234, null)
+      if (json && typeof json === 'object') {
+        return json;
+      }
+      setError(`${_value.substring(0, 8)} is not valid JSON`);
+    }
+  } catch (e) {
+    setError(e.message);
+  }
+  return false;
+};
+
+storiesOf('Watson IoT/CardCodeEditor', module)
+  .add('default', () => (
+    <CardCodeEditor
+      onSubmit={isValidCallback}
+      onClose={() => {}}
+      onCopy={value => console.log(value)}
+      initialValue="// write your code here"
+    />
+  ))
+  .add('with preloaded content', () => (
+    <CardCodeEditor
+      onSubmit={isValidCallback}
+      onClose={() => {}}
+      onCopy={value => console.log(value)}
+      initialValue={JSON.stringify(pkgjson, null, 2)}
+    />
+  ));
