ada66300066b8fa2221943f371014f694d90cb0e
jonguenther <me@jontg.eu>
2020-11-11 18:25:53 +0100

    fix(emptystate): remove mistake

--- src/components/EmptyState/EmptyState.jsx
+++ src/components/EmptyState/EmptyState.jsx
@@ -1,13 +1,3 @@
-/**
- * IBM Confidential
- * OCO Source Materials
- * 5900-A0N
- * © Copyright IBM Corp. 2020
- * The source code for this program is not published or otherwise divested of its
- * trade secrets, irrespective of what has been deposited with the U.S. Copyright
- * Office.
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
