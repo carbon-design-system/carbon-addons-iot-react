a836b3ece5efef628f08514c977c903ff7579640
Cal Smith <calsmith@protonmail.com>
2020-11-26 13:46:21 -0500

    fix publish script

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,8 +12,6 @@ if [[ $TRAVIS_BRANCH == "master" ]]; then
   lerna version --conventional-commits --conventional-graduate --create-release github --yes
   # publish the packages that were just versioned
   lerna publish from-package --dist-tag next --yes
-  # push changes back to next so we can start work on the next release
-  git push origin next
 fi
 
 if [[ $TRAVIS_BRANCH == "next" ]]; then
