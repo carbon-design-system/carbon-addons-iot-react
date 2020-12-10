e1c678a71ae5a9db9cd9196011380f5bc8e38763
Bjorn Alm <bjorn.b.alm@gmail.com>
2020-12-10 21:54:06 +0100

    feat(hotspot): fix linting

--- src/components/ImageCard/HotspotContent.test.jsx
+++ src/components/ImageCard/HotspotContent.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { red60 } from '@carbon/colors';
 
 import HotspotContent from './HotspotContent';
-import userEvent from '@testing-library/user-event';
 
 describe('HotspotContent', () => {
   it('basic attributes', () => {
