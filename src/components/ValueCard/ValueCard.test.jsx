a149853fff3433d0949298cd81af7bd4dd409e8c
Stephen Stone <stephen.m.stone@ibm.com>
2021-01-15 15:39:43 -0600

    test(valuecard): add tests for util functions

--- src/components/ValueCard/ValueCard.test.jsx
+++ src/components/ValueCard/ValueCard.test.jsx
@@ -2,51 +2,14 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { screen, render } from '@testing-library/react';
 
-import {
-  CARD_LAYOUTS,
-  CARD_DATA_STATE,
-  CARD_SIZES,
-} from '../../constants/LayoutConstants';
+import { CARD_DATA_STATE, CARD_SIZES } from '../../constants/LayoutConstants';
 import { settings } from '../../constants/Settings';
 
-import Attribute from './Attribute';
 import ValueCard from './ValueCard';
 
 const { iotPrefix } = settings;
 
 describe('ValueCard', () => {
-  it('fail over to vertical layouts when not enough space', () => {
-    const wrapper = mount(
-      <ValueCard
-        content={{ attributes: [{ label: 'title', dataSourceId: 'v' }] }}
-        values={{ v: 'value' }}
-        size={CARD_SIZES.LARGE}
-      />
-    );
-    expect(wrapper.find(Attribute).prop('layout')).toEqual(
-      CARD_LAYOUTS.HORIZONTAL
-    );
-
-    const wrapper2 = mount(
-      <ValueCard
-        title="Something"
-        content={{
-          attributes: [
-            { label: 'title', dataSourceId: 'v1' },
-            { label: 'title2', dataSourceId: 'v2' },
-            { label: 'title3', dataSourceId: 'v3' },
-            { label: 'title4', dataSourceId: 'v4' },
-          ],
-        }}
-        size={CARD_SIZES.LARGE}
-        values={{ v: 'value' }}
-      />
-    );
-    expect(wrapper2.find(Attribute).first().prop('layout')).toEqual(
-      CARD_LAYOUTS.VERTICAL
-    );
-  });
-
   it('DataState prop shows DataState elements instead of content', () => {
     const wrapperWithoutDataState = mount(
       <ValueCard
--- src/components/ValueCard/valueCardUtils.js
+++ src/components/ValueCard/valueCardUtils.js
@@ -8,38 +8,6 @@ const { iotPrefix } = settings;
 export const BASE_CLASS_NAME = `${iotPrefix}--value-card`;
 export const PREVIEW_DATA = '--';
 
-/** Determine the max value card attribute count */
-// TODO: this is no longer valid with new design, but needs to stay because its being exported
-export const determineMaxValueCardAttributeCount = (
-  size,
-  currentAttributeCount
-) => {
-  // console.warn('DEPRECATION: This function is no longer valid.');
-  let attributeCount = currentAttributeCount;
-  switch (size) {
-    case CARD_SIZES.SMALL:
-      attributeCount = 1;
-      break;
-    case CARD_SIZES.SMALLWIDE:
-      attributeCount = 2;
-      break;
-    case CARD_SIZES.MEDIUMTHIN:
-    case CARD_SIZES.MEDIUM:
-    case CARD_SIZES.MEDIUMWIDE:
-      attributeCount = 3;
-      break;
-    case CARD_SIZES.LARGE:
-      attributeCount = 5;
-      break;
-    case CARD_SIZES.LARGETHIN:
-    case CARD_SIZES.LARGEWIDE:
-      attributeCount = 7;
-      break;
-    default:
-  }
-  return attributeCount;
-};
-
 /**
  * Support either an array of values or an object of values
  * @param {string} dataSourceId
