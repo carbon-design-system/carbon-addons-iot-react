815be49613fd3c7481caf08c480731bfd70c874b
Cal Smith <calsmith@protonmail.com>
2021-01-22 16:37:03 -0500

    fix next/latest in publish

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,14 +10,14 @@ if [[ $GITHUB_REF =~ "master" ]]; then
   # graduate the relase with --conventional-graduate
   lerna version --conventional-commits --conventional-graduate --create-release github --yes
   # publish the packages that were just versioned
-  lerna publish from-git --dist-tag next --yes
+  lerna publish from-git --dist-tag latest --yes
 fi
 
 if [[ $GITHUB_REF =~ "next" ]]; then
   # version a prerelease to the `next` dist-tag with the `next` preid
   lerna version --conventional-commits --conventional-prerelease --preid next --create-release github --yes
   # publish the packages that were just versioned
-  lerna publish from-git --dist-tag latest --yes
+  lerna publish from-git --dist-tag next --yes
 fi
 
 # just to be sure we exit cleanly
