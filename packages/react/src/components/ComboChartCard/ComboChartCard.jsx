143d6d12a75601667dc9f07fd9bbcd58cb4d9dbb
David Conner <rdconner@us.ibm.com>
2021-01-28 11:09:14 -0500

    feat(combochartcard): add proptypes for combochart

--- packages/react/src/components/ComboChartCard/ComboChartCard.jsx
+++ packages/react/src/components/ComboChartCard/ComboChartCard.jsx
@@ -1,7 +1,6 @@
 /* istanbul ignore file */
 // Ignoring until we resolve the issue with importing this component in jest
 import React from 'react';
-import PropTypes from 'prop-types';
 import { ComboChart } from '@carbon/charts-react';
 import classnames from 'classnames';
 import isEmpty from 'lodash/isEmpty';
@@ -10,25 +9,27 @@ import { CARD_SIZES } from '../../constants/LayoutConstants';
 import Card from '../Card/Card';
 import { CardPropTypes } from '../../constants/CardPropTypes';
 import { settings } from '../../constants/Settings';
+import StatefulTable from '../Table/StatefulTable';
 
 const { iotPrefix } = settings;
 
-const propTypes = {
-  id: PropTypes.string.isRequired,
-  i18n: PropTypes.shape({
-    noData: PropTypes.string,
-  }),
-};
 
 const defaultProps = {
   size: CARD_SIZES.MEDIUMWIDE,
+  locale: 'en',
   title: '',
   i18n: {
     noDataLabel: 'No data',
   },
 };
 
-const ComboChartCard = ({ className, title, values, options, ...others }) => {
+const ComboChartCard = ({
+  className,
+  title,
+  values,
+  options,
+  ...others
+}) => {
   const { title: chartTitle, ...otherOptions } = options;
 
   return (
@@ -40,6 +41,44 @@ const ComboChartCard = ({ className, title, values, options, ...others }) => {
     >
       <div className={`${iotPrefix}--combo-chart-card__container`}>
         <ComboChart key="combo-chart" data={values} {...others} options={otherOptions} />
+        {isExpanded ? (
+            <StatefulTable
+              id="BarChartCard-table"
+              className={`${iotPrefix}--bar-chart-card--stateful-table`}
+              columns={tableColumns}
+              data={tableData}
+              options={{
+                hasPagination: true,
+                hasSearch: true,
+                hasFilter: true,
+              }}
+              actions={{
+                toolbar: {
+                  onDownloadCSV: (filteredData) => csvDownloadHandler(filteredData, title),
+                },
+              }}
+              view={{
+                pagination: {
+                  pageSize: 10,
+                  pageSizes: [10, 20, 30],
+                },
+                toolbar: {
+                  activeBar: null,
+                },
+                filters: [],
+                table: {
+                  sort: {
+                    columnId: timeDataSourceId,
+                    direction: 'DESC',
+                  },
+                  emptyState: {
+                    message: noDataLabel,
+                  },
+                },
+              }}
+              i18n={i18n}
+            />
+          ) : null}
       </div>
     </Card>
   );
