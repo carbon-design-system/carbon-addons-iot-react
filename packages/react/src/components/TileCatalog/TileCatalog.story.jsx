8ae0cffb6a6cf8b0490023a8edf1855f8ef17d17
David Conner <rdconner@us.ibm.com>
2021-02-12 12:06:48 -0500

    feat(repo): pull in next to maste and fix memory leak

--- packages/react/src/components/TileCatalog/TileCatalog.story.jsx
+++ packages/react/src/components/TileCatalog/TileCatalog.story.jsx
@@ -132,10 +132,12 @@ export const AsyncLoadedWaitOneSecond = () => {
     const [tilesProp, setTiles] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
-      setTimeout(() => {
+      const timer = setTimeout(() => {
         setTiles(commonTileCatalogProps.tiles);
         setIsLoading(false);
       }, 1000);
+
+      return () => clearTimeout(timer);
     }, []);
     return (
       <StatefulTileCatalog {...commonTileCatalogProps} isLoading={isLoading} tiles={tilesProp} />
