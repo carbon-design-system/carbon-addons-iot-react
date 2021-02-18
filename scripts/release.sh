156f9bf79112c0fd1b79784afebe6297c0fdacd2
Cal Smith <calsmith@protonmail.com>
2021-02-18 14:38:45 -0500

    switch over to carbon-bot and CARBON_BOT_TOKEN

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,18 +2,18 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-if [[ $GITHUB_ACTOR == "cal-smith" ]]; then
+if [[ $GITHUB_ACTOR == "carbon-bot" ]]; then
   # exit early, since we don't want to try publishing _again_
   exit 0;
 fi
 
 # set username and email so git knows who we are
-git config user.name "cal-smith"
-git config user.email "callums@ca.ibm.com"
+git config user.name "carbon-bot"
+git config user.email "carbon@us.ibm.com"
 
 # Add github token to git credentials
 git config credential.helper "store --file=.git/credentials"
-echo "https://${ADMIN_TOKEN}:@github.com" > .git/credentials 2>/dev/null
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials 2>/dev/null
 
 # authenticate with the npm registry
 npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
