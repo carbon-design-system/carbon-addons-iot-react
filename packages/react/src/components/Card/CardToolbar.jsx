72a23efbb321a9dc18774d3189139c19d31a25a4
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2021-02-28 13:30:39 -0700

    fix(cardtoolbar): need to add tooltips to the clone and delete

--- packages/react/src/components/Card/CardToolbar.jsx
+++ packages/react/src/components/Card/CardToolbar.jsx
@@ -133,13 +133,19 @@ const CardToolbar = ({
   return isEditable ? (
     <div className={classnames(className, `${iotPrefix}--card--toolbar`)}>
       {(availableActions.clone || availableActions.delete) && (
-        <OverflowMenu flipped title={mergedI18n.overflowMenuDescription} iconDescription={mergedI18n.overflowMenuDescription}>
+        <OverflowMenu
+          flipped
+          title={mergedI18n.overflowMenuDescription}
+          iconDescription={mergedI18n.overflowMenuDescription}
+        >
           {availableActions.clone && (
             <OverflowMenuItem
               onClick={() => {
                 onCardAction(CARD_ACTIONS.CLONE_CARD);
               }}
               itemText={mergedI18n.cloneCardLabel}
+              title={mergedI18n.cloneCardLabel}
+              requireTitle
             />
           )}
           {availableActions.delete && (
@@ -149,6 +155,8 @@ const CardToolbar = ({
                 onCardAction(CARD_ACTIONS.DELETE_CARD);
               }}
               itemText={mergedI18n.deleteCardLabel}
+              title={mergedI18n.deleteCardLabel}
+              requireTitle
             />
           )}
         </OverflowMenu>
