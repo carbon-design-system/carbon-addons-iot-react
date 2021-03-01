6020f3b89442eccd6340f9f07aee5bfb5ae09a2c
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2021-03-01 14:48:47 -0700

    fix(dashboardeditor): data filter was not showing

--- packages/react/src/components/DashboardEditor/DashboardEditor.story.jsx
+++ packages/react/src/components/DashboardEditor/DashboardEditor.story.jsx
@@ -403,9 +403,11 @@ export const SummaryDashboardWithInitialValue = () => (
       dataItems={mockDataItems.map((item) => ({ ...item, grain: 'Weekly' }))}
       availableDimensions={{
         firmware: ['1.2', '1.3'],
+        axes: [1, 2, 3],
+        deviceid: ['73004', '73000', '73001'],
       }}
+      isSummaryDashboard
       initialValue={{
-        isSummaryDashboard: true,
         cards: [
           {
             id: 'Table',
