f38076d3a9fdfb40834e73fb17f29e94e9fabe9d
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2021-03-01 11:50:33 -0700

    fix(tablecard): need to regenerate the table card if the number of columns change

--- packages/react/src/components/TableCard/TableCard.jsx
+++ packages/react/src/components/TableCard/TableCard.jsx
@@ -640,12 +640,13 @@ const TableCard = ({
       {...others}
     >
       {({ height }) => {
-        const numberOfRowsPerPage = !isNil(height) ? Math.floor((height - 48 * 3) / 48) : 10;
+        const numberOfRowsPerPage = Math.max(!isNil(height) && Math.floor((height - 48) / 48), 10);
         return (
           <StyledStatefulTable
             columns={columnsToRender}
             data={tableDataWithTimestamp}
             id={`table-for-card-${id}`}
+            key={`table-for-card-${numberOfRowsPerPage}-${columnsToRender?.length}`}
             isExpanded={isExpanded}
             secondaryTitle={title}
             tooltip={tooltip}
