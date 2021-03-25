b4a61f428df5af5e9698c01147e1f33e1714a1cd
Kevin Perrine <kperrine@gmail.com>
2021-03-24 20:49:38 -0400

    chore(image-hotspots): raise test coverage, remove exception

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
       functions: 80,
       lines: 80,
     },
-    './src/components/**/!(ColumnHeaderSelect|WizardInline|WizardHeader|FilterHeaderRow|RowActionsCell|RowActionsError|TableHead|StatefulTable|StatefulTableDetailWizard|CatalogContent|FileDrop|Dashboard|CardRenderer|ImageHotspots|PageHero|PageTitle|EditPage|TableHead|ColumnResize|ColorDropdown|TimeSeriesCard|BarChartCard|DashboardGrid|DashboardEditor).jsx': {
+    './src/components/**/!(ColumnHeaderSelect|WizardInline|WizardHeader|FilterHeaderRow|RowActionsCell|RowActionsError|TableHead|StatefulTable|StatefulTableDetailWizard|CatalogContent|FileDrop|Dashboard|CardRenderer|PageHero|PageTitle|EditPage|TableHead|ColumnResize|ColorDropdown|TimeSeriesCard|BarChartCard|DashboardGrid|DashboardEditor).jsx': {
       statements: 80,
       branches: 80,
       functions: 80,
@@ -79,9 +79,6 @@ module.exports = {
       branches: 38,
       lines: 51,
       functions: 66,
-    },
-    './src/components/ImageCard/ImageHotspots.jsx': {
-      branches: 79,
     },
     './src/components/Page/EditPage.jsx': {
       statements: 63,
