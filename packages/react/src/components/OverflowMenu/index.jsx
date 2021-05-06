e286133aa754643408a7c855ddcd5052be8b1f97
Kevin Perrine <kperrine@gmail.com>
2021-05-06 09:20:43 -0400

    fix(overflow-menu): change import structure for getMenuOffset

--- packages/react/src/components/OverflowMenu/index.jsx
+++ packages/react/src/components/OverflowMenu/index.jsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { OverflowMenu as CarbonOverflowMenu } from 'carbon-components-react';
 import PropTypes from 'prop-types';
-import { getMenuOffset } from 'carbon-components-react/es/components/OverflowMenu/OverflowMenu';
+// no idea why import { getMenuOffset } won't work, but it causes an 'getMenuOffset' is not exported by node_modules/carbon-components-react/lib/components/OverflowMenu/OverflowMenu.js error
+import * as OM from 'carbon-components-react/lib/components/OverflowMenu/OverflowMenu';
 
 import { usePopoverPositioning } from '../../hooks/usePopoverPositioning';
 
 export { OverflowMenuItem } from 'carbon-components-react';
+
+const { getMenuOffset } = OM;
 
 export const OverflowMenu = ({ direction, menuOffset, useAutoPositioning, flipped, ...props }) => {
   const [calculateMenuOffset, { adjustedDirection, adjustedFlipped }] = usePopoverPositioning({
