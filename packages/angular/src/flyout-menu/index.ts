d8f4611dcaf8f918346006a819b247d11ffb408c
Zvonimir Fras <zvonimir.fras@ibm.com>
2021-05-31 15:21:20 -0400

    chore(flyout-menu): add exports

--- packages/angular/src/flyout-menu/index.ts
+++ packages/angular/src/flyout-menu/index.ts
@@ -1,3 +1,4 @@
 export { FlyoutMenu } from './flyout-menu.component';
 export { FlyoutMenuPane } from './flyout-menu-pane.component';
+export { FlyoutMenuDirective } from './flyout-menu.directive';
 export { FlyoutMenuModule } from './flyout-menu.module';
--- packages/angular/src/index.ts
+++ packages/angular/src/index.ts
@@ -1,2 +1,3 @@
 export * from '@ai-apps/angular/card';
 export * from '@ai-apps/angular/toolkit';
+export * from '@ai-apps/angular/flyout-menu';
