88a2ec100ce39c4c9a73877dc9a9d2ec220283b8
Kevin Perrine <kperrine@gmail.com>
2021-06-30 10:54:02 -0400

    chore(repo): fix linting errors from test cleanup

--- packages/react/src/components/Table/TableHead/ColumnHeaderRow/ColumnHeaderRow.test.jsx
+++ packages/react/src/components/Table/TableHead/ColumnHeaderRow/ColumnHeaderRow.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { mount } from 'enzyme';
 import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
