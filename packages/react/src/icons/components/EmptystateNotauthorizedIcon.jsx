8579409ed57aac654b743ccfca778aa7eadff55a
Zvonimir Fras <zvonimir.fras@ibm.com>
2021-07-16 10:40:56 -0400

    Merge branch 'next' of git://github.com/carbon-design-system/carbon-addons-iot-react into side-panel

--- packages/react/src/icons/components/EmptystateNotauthorizedIcon.jsx
+++ packages/react/src/icons/components/EmptystateNotauthorizedIcon.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 function SvgEmptystateNotauthorizedIcon(props) {
   return (
-    <svg width={80} height={80} {...props}>
+    <svg width={80} height={80} xmlns="http://www.w3.org/2000/svg" {...props}>
       <defs>
         <radialGradient
           cx="47.29%"
