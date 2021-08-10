26d8f466877609dfede91cb829325c6df18fd2af
Kevin Perrine <kperrine@gmail.com>
2021-08-10 10:55:34 -0400

    fix(bar-chart-card): format x-axis labels to match TimeSeriesCard

--- packages/react/src/components/BarChartCard/BarChartCard.story.jsx
+++ packages/react/src/components/BarChartCard/BarChartCard.story.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { text, select, object, boolean } from '@storybook/addon-knobs';
+import { text, select, object, boolean, number } from '@storybook/addon-knobs';
 
 import { CARD_SIZES, BAR_CHART_TYPES, BAR_CHART_LAYOUTS } from '../../constants/LayoutConstants';
 import { getCardMinSize } from '../../utils/componentUtilityFunctions';
 import { barChartData } from '../../utils/barChartDataSample';
+import { getIntervalChartData } from '../../utils/sample';
 
 import BarChartCard from './BarChartCard';
 import BarChartCardREADME from './BarChartCard.mdx';
@@ -134,6 +135,74 @@ export const SimpleBarTimeSeriesCustomDomainRange = () => {
 
 SimpleBarTimeSeriesCustomDomainRange.storyName =
   'with simple bar of time series data and custom domainRange';
+
+export const SimpleBarTimeSeries = () => {
+  const size = select('size', sizes, CARD_SIZES.MEDIUMWIDE);
+  const breakpoint = select('breakpoint', ['lg', 'md', 'sm', 'xs'], 'lg');
+  const interval = select(
+    'interval',
+    ['minute', 'hour', 'day', 'week', 'quarter', 'month', 'year'],
+    'minute'
+  );
+  return (
+    <div style={{ width: `${getCardMinSize(breakpoint, size).x}px`, margin: 20 }}>
+      <BarChartCard
+        title={text('title', 'Temperature over time')}
+        id="simple-time-sample"
+        breakpoint={breakpoint}
+        isLoading={boolean('isLoading', false)}
+        isEditable={boolean('isEditable', false)}
+        isExpanded={boolean('isExpanded', false)}
+        content={{
+          ...object('content', {
+            xLabel: 'Date',
+            yLabel: 'Temperature',
+            series: [
+              {
+                dataSourceId: 'temperature',
+
+                label: 'Temperature really long label to check trunc',
+              },
+            ],
+            timeDataSourceId: 'timestamp',
+            type: 'SIMPLE',
+          }),
+          layout: select('content.layout', layouts, BAR_CHART_LAYOUTS.VERTICAL),
+          zoomBar: {
+            enabled: boolean('content.zoomBar.enabled', false),
+            axes: 'top',
+            view: select('content.zoomBar.view', ['slider_view', 'graph_view'], 'slider_view'),
+          },
+        }}
+        // key is a simple storybook hack to force the chart to re-render when zoomBar changes,
+        // otherwise carbon-charts won't automatically re-render it (even in their own stories).
+        key={`${boolean('content.zoomBar.enabled', false)}-${select(
+          'content.layout',
+          layouts,
+          BAR_CHART_LAYOUTS.VERTICAL
+        )}`}
+        i18n={object('i18n', {
+          noDataLabel: 'No data for this card.',
+        })}
+        values={getIntervalChartData(
+          interval,
+          number('numberOfDataPoints', 15),
+          {
+            min: number('minimumDataPointRange', 10),
+            max: number('maximumDataPointRange', 100),
+          },
+          10
+        )}
+        interval={interval}
+        size={size}
+        onCardAction={action('onCardAction')}
+        availableActions={object('availableActions', { expand: true })}
+      />
+    </div>
+  );
+};
+
+SimpleBarTimeSeries.storyName = 'with simple bar of time series data and intervals';
 
 export const GroupedBar = () => {
   const size = select('size', sizes, CARD_SIZES.MEDIUMWIDE);
