7a22adac92d8d43928b1773e474e7512688204b5
Cal Smith <calsmith@protonmail.com>
2021-08-17 15:38:47 -0400

    fix initial review comments

--- packages/angular/src/table/head/table-head-cell.component.ts
+++ packages/angular/src/table/head/table-head-cell.component.ts
@@ -34,34 +34,8 @@ import { TableHeadCell } from 'carbon-components-angular';
         >
         </ng-template>
         <span class="table-head-cell-icons">
-          <svg
-            focusable="false"
-            preserveAspectRatio="xMidYMid meet"
-            style="will-change: transform;"
-            xmlns="http://www.w3.org/2000/svg"
-            class="bx--table-sort__icon"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            aria-hidden="true"
-          >
-            <path d="M12.3 9.3l-3.8 3.8V1h-1v12.1L3.7 9.3 3 10l5 5 5-5z"></path>
-          </svg>
-          <svg
-            focusable="false"
-            preserveAspectRatio="xMidYMid meet"
-            style="will-change: transform;"
-            xmlns="http://www.w3.org/2000/svg"
-            class="bx--table-sort__icon-unsorted"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            aria-hidden="true"
-          >
-            <path
-              d="M13.8 10.3L12 12.1V2h-1v10.1l-1.8-1.8-.7.7 3 3 3-3zM4.5 2l-3 3 .7.7L4 3.9V14h1V3.9l1.8 1.8.7-.7z"
-            ></path>
-          </svg>
+          <svg ibmIcon="arrow--down" size="16" class="bx--table-sort__icon"></svg>
+          <svg ibmIcon="arrows--vertical" size="16" class="bx--table-sort__icon-unsorted"></svg>
         </span>
       </button>
       <span
@@ -89,15 +63,7 @@ import { TableHeadCell } from 'carbon-components-angular';
         placement="bottom,top"
         [data]="column.filterData"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="icon--sm"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-        >
-          <path d="M0 0v3l6 8v5h4v-5l6-8V0H0zm9 10.7V15H7v-4.3L1.3 3h13.5L9 10.7z" />
-        </svg>
+        <svg ibmIcon="filter" size="16" class="icon--sm"></svg>
         <span *ngIf="column.filterCount > 0">
           {{ column.filterCount }}
         </span>
