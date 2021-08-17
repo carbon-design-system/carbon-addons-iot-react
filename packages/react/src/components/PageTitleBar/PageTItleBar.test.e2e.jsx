70d0130ee2399b513ceea758cf5d55721fa686b1
Kevin Perrine <kperrine@gmail.com>
2021-08-17 17:50:27 -0400

    chore(testing): add css properties into cypress tests

--- packages/react/src/components/PageTitleBar/PageTItleBar.test.e2e.jsx
+++ packages/react/src/components/PageTitleBar/PageTItleBar.test.e2e.jsx
@@ -11,38 +11,29 @@ describe('PageTitleBar', () => {
           breadcrumb={[<a href="/">Home</a>, <a href="/">Type</a>, <span>Instance</span>]}
           title="testTitle"
           headerMode="DYNAMIC"
-          description={<p data-testid="test-description">test</p>}
           testId="page-title-bar"
-          upperActions={
-            <button data-testid="upper-action-delete" type="button">
-              Delete
-            </button>
-          }
         />
       </div>
     );
 
-    cy.findByTestId('test-description').should('have.text', 'test');
-    cy.findByTestId('upper-action-delete').should('have.text', 'Delete');
     cy.findByTestId('page-title-bar')
       .should('not.have.class', 'page-title-bar--dynamic--during')
       .should('not.have.class', 'page-title-bar--dynamic--after')
-      .should('not.have.class', 'page-title-bar--dynamic--before');
-    // .should('have.css', '--header-offset', '48px')
-    // .should('have.css', '--scroll-transition-progress', '0');
+      .should('not.have.class', 'page-title-bar--dynamic--before')
+      .should('have.attr', 'style', '--header-offset:48px; --scroll-transition-progress:0;');
 
     cy.scrollTo(0, 118);
     cy.findByTestId('page-title-bar')
       .should('have.class', 'page-title-bar--dynamic--during')
       .should('not.have.class', 'page-title-bar--dynamic--after')
-      .should('not.have.class', 'page-title-bar--dynamic--before');
-    // .should('have.css', '--scroll-transition-progress', '0.2');
+      .should('not.have.class', 'page-title-bar--dynamic--before')
+      .should('have.attr', 'style', '--header-offset:48px; --scroll-transition-progress:0.2;');
 
     cy.scrollTo(0, 200);
     cy.findByTestId('page-title-bar')
       .should('not.have.class', 'page-title-bar--dynamic--during')
       .should('have.class', 'page-title-bar--dynamic--after')
-      .should('not.have.class', 'page-title-bar--dynamic--before');
-    // .should('have.css', '--scroll-transition-progress', '1');
+      .should('not.have.class', 'page-title-bar--dynamic--before')
+      .should('have.attr', 'style', '--header-offset:48px; --scroll-transition-progress:1;');
   });
 });
