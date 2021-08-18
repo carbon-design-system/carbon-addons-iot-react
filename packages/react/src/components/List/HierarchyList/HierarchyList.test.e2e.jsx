fb3e071d73ec1bb34f2c8ff8f07c52cea67cb903
Kevin Perrine <kperrine@gmail.com>
2021-08-18 15:57:52 -0400

    chore(testing): remove players, add tests to cover other scenarios

--- packages/react/src/components/List/HierarchyList/HierarchyList.test.e2e.jsx
+++ packages/react/src/components/List/HierarchyList/HierarchyList.test.e2e.jsx
@@ -10,25 +10,13 @@ const sampleHierarchy = {
         'Leury Garcia': 'CF',
         'Yoan Moncada': '3B',
         'Jose Abreu': '1B',
-        'Welington Castillo': 'C',
-        'Eloy Jimenez': 'LF',
-        'Charlie Tilson': 'RF',
-        'Tim Anderson': 'SS',
-        'Yolmer Sanchez': '2B',
-        'Dylan Covey': 'P',
       },
     },
     'National League': {
       'New York Mets': {
-        'Jeff McNeil': '3B',
         'Amed Rosario': 'SS',
         'Michael Conforto is a super duper long name that will get cut off': 'RF',
         'Pete Alonso': '1B',
-        'Wilson Ramos': 'C',
-        'Robinson Cano': '2B',
-        'JD Davis': 'LF',
-        'Brandon Nimmo': 'CF',
-        'Jacob Degrom': 'P',
       },
     },
   },
@@ -66,7 +54,7 @@ const getInitialItems = () => [
 ];
 
 describe('HierarchyList', () => {
-  it('handles drag and drop with no selections', () => {
+  it('handles drag and drop above and below with no selections', () => {
     const onSelect = cy.stub();
     const onListUpdated = cy.stub();
     mount(
@@ -87,20 +75,33 @@ describe('HierarchyList', () => {
       </div>
     );
 
-    // expect Pete in position 6 and Amed in position 4 (zero-based-index)
-    cy.get('.iot--list-item').eq(5).find('[title]').should('have.text', 'Pete Alonso');
-    cy.get('.iot--list-item').eq(3).find('[title]').should('have.text', 'Amed Rosario');
+    // expect Pete in position 5 and Amed in position 3 (zero-based-index)
+    cy.get('.iot--list-item').eq(4).find('[title]').should('have.text', 'Pete Alonso');
+    cy.get('.iot--list-item').eq(2).find('[title]').should('have.text', 'Amed Rosario');
 
-    // Select Pete and drag him to Amed (one-based-index)
-    cy.get(':nth-child(6) > [draggable="true"]')
-      .drag(':nth-child(4) > [draggable="true"]')
+    // Select Pete and drag above to Amed (one-based-index)
+    cy.findByTitle('Pete Alonso')
+      .drag(':nth-child(3) > [draggable="true"]', { position: 'top' })
       .then(() => {
         expect(onListUpdated).to.be.called;
       });
 
-    // expect Pete in position 4 and Amed in position 5 (zero-based-index)
-    cy.get('.iot--list-item').eq(3).find('[title]').should('have.text', 'Pete Alonso');
-    cy.get('.iot--list-item').eq(4).find('[title]').should('have.text', 'Amed Rosario');
+    // expect Pete in position 3 and Amed in position 4 (zero-based-index)
+    cy.get('.iot--list-item').eq(2).find('[title]').should('have.text', 'Pete Alonso');
+    cy.get('.iot--list-item').eq(3).find('[title]').should('have.text', 'Amed Rosario');
+
+    // drag michael to the bottom of the list.
+    cy.findByTitle('Michael Conforto is a super duper long name that will get cut off')
+      .drag(':nth-child(5) > [draggable="true"]', { position: 'bottom' })
+      .then(() => {
+        expect(onListUpdated).to.be.called;
+      });
+
+    // expect Michael to be in position 5
+    cy.get('.iot--list-item')
+      .eq(4)
+      .find('[title]')
+      .should('have.text', 'Michael Conforto is a super duper long name that will get cut off');
   });
 
   it('handles drag and drop with a selection', () => {
@@ -124,19 +125,99 @@ describe('HierarchyList', () => {
       </div>
     );
 
-    // expect Pete in position 6 and Amed in position 4 (zero-based-index)
-    cy.get('.iot--list-item').eq(5).find('[title]').should('have.text', 'Pete Alonso');
-    cy.get('.iot--list-item').eq(3).find('[title]').should('have.text', 'Amed Rosario');
+    // expect Pete in position 5 and Amed in position 3 (zero-based-index)
+    cy.get('.iot--list-item').eq(4).find('[title]').should('have.text', 'Pete Alonso');
+    cy.get('.iot--list-item').eq(2).find('[title]').should('have.text', 'Amed Rosario');
 
-    // Select Pete and drag him to Amed (one-based-index)
-    cy.get(':nth-child(6) > [draggable="true"]')
-      .drag(':nth-child(4) > [draggable="true"]')
+    // Select Pete and drag above to Amed (one-based-index)
+    cy.findByTitle('Pete Alonso')
+      .drag(':nth-child(3) > [draggable="true"]', { position: 'top' })
       .then(() => {
         expect(onListUpdated).to.be.called;
       });
 
+    // expect Pete in position 3 and Amed in position 4 (zero-based-index)
+    cy.get('.iot--list-item').eq(2).find('[title]').should('have.text', 'Pete Alonso');
+    cy.get('.iot--list-item').eq(3).find('[title]').should('have.text', 'Amed Rosario');
+  });
+
+  it('handles drag and drop on-top to create a new category with single-nesting', () => {
+    const onSelect = cy.stub();
+    const onListUpdated = cy.stub();
+    mount(
+      <div style={{ width: 400, height: 600 }}>
+        <HierarchyList
+          title="MLB Expanded List"
+          items={getInitialItems()}
+          defaultSelectedId="New York Mets_Pete Alonso"
+          editingStyle="single-nesting"
+          pageSize="lg"
+          isLoading={false}
+          isLargeRow={false}
+          onListUpdated={onListUpdated}
+          hasSearch
+          hasDeselection
+          onSelect={onSelect}
+        />
+      </div>
+    );
+
+    // expect Pete in position 5 and Amed in position 3 (zero-based-index)
+    cy.get('.iot--list-item').eq(4).find('[title]').should('have.text', 'Pete Alonso');
+
+    // Select Pete and drag him to Amed (one-based-index)
+    cy.findByTitle('Pete Alonso')
+      .drag(':nth-child(4) > [draggable="true"]', { position: 'left' })
+      .then(() => {
+        expect(onListUpdated).to.be.called;
+      });
+
+    // open the newly created nesting
+    cy.findAllByTestId('expand-icon').eq(2).click();
+
     // expect Pete in position 4 and Amed in position 5 (zero-based-index)
-    cy.get('.iot--list-item').eq(3).find('[title]').should('have.text', 'Pete Alonso');
-    cy.get('.iot--list-item').eq(4).find('[title]').should('have.text', 'Amed Rosario');
+    cy.get('.iot--list-item').eq(4).find('[title]').should('have.text', 'Pete Alonso');
+  });
+
+  it('handles drag and drop into a category with multiple-nesting', () => {
+    const onSelect = cy.stub();
+    const onListUpdated = cy.stub();
+    mount(
+      <div style={{ width: 400, height: 600 }}>
+        <HierarchyList
+          title="MLB Expanded List"
+          items={getInitialItems()}
+          defaultSelectedId="New York Mets_Pete Alonso"
+          editingStyle="multiple-nesting"
+          pageSize="lg"
+          isLoading={false}
+          isLargeRow={false}
+          onListUpdated={onListUpdated}
+          hasSearch
+          hasDeselection
+          onSelect={onSelect}
+        />
+      </div>
+    );
+
+    // expect Pete in position 5 and Amed in position 3 (zero-based-index)
+    cy.get('.iot--list-item').eq(4).find('[title]').should('have.text', 'Pete Alonso');
+    cy.get('.iot--list-item').eq(2).find('[title]').should('have.text', 'Amed Rosario');
+
+    // select Amed to move him, too.
+    cy.findByTestId('New York Mets_Amed Rosario-checkbox').click({ force: true });
+
+    // Select Pete and drag him to the Chicago White Sox (one-based-index)
+    cy.findByTitle('Pete Alonso')
+      .drag(':nth-child(1) > [draggable="true"]', { position: 'left' })
+      .then(() => {
+        expect(onListUpdated).to.be.called;
+      });
+
+    cy.findAllByTestId('expand-icon').eq(0).click();
+
+    // expect Pete in position 3 and Amed in position 2 (zero-based-index)
+    cy.get('.iot--list-item').eq(1).find('[title]').should('have.text', 'Amed Rosario');
+    cy.get('.iot--list-item').eq(2).find('[title]').should('have.text', 'Pete Alonso');
   });
 });
