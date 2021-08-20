ebc0cdc3a46fc3d8b1821a7e840c61bedb242d5c
Bjorn Alm <bjorn.b.alm@gmail.com>
2021-08-20 17:02:38 +0200

    test(dashboardheader): set individual thresholds to current coverage

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -43,6 +43,8 @@ module.exports = {
       // https://github.com/carbon-design-system/carbon-charts/issues/594
       functions: 77,
     },
+    // Deprecated, no need for thresholds to be at 90%
+    './src/components/Dashboard/DashboardHeader.jsx': { branches: 83 },
   },
   globals: {
     __DEV__: false,
