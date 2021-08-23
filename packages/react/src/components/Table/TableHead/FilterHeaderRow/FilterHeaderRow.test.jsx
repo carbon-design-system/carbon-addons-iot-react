d56d148958720dcd540feee6f102c1a6bd586e3e
Kevin Perrine <kperrine@gmail.com>
2021-08-23 18:37:55 -0400

    chore(testing): remove enzyme

--- packages/react/src/components/Table/TableHead/FilterHeaderRow/FilterHeaderRow.test.jsx
+++ packages/react/src/components/Table/TableHead/FilterHeaderRow/FilterHeaderRow.test.jsx
@@ -1,6 +1,4 @@
-import { mount } from 'enzyme';
 import React from 'react';
-import { ComboBox, TextInput } from 'carbon-components-react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
@@ -37,76 +35,76 @@ describe('FilterHeaderRow', () => {
   });
 
   it('text input change updates state', () => {
-    const wrapper = mount(
+    render(
       <FilterHeaderRow
         {...commonFilterProps}
         ordering={[{ columnId: 'col1' }]}
         columns={[{ id: 'col1', isFilterable: true }]}
       />
     );
-    wrapper.find('input').simulate('change', { target: { value: 'mytext' } });
-    expect(wrapper.state()).toEqual({
-      filterValues: { col1: 'mytext' },
-      prevPropsFilters: [],
-    });
+
+    const input = screen.getByPlaceholderText('Type and hit enter to apply');
+    userEvent.type(input, 'mytext');
+    expect(input).toHaveValue('mytext');
   });
 
   it('each column is marked with data-column', () => {
-    const wrapper = mount(
+    const { container } = render(
       <FilterHeaderRow
         {...commonFilterProps}
         ordering={[{ columnId: 'col1' }]}
         columns={[{ id: 'col1' }]}
       />
     );
-    const columns = wrapper.find("th[data-column='col1']");
+    const columns = container.querySelectorAll("th[data-column='col1']");
     expect(columns).toHaveLength(1);
   });
 
   it('text input clear button clears filter', () => {
-    const wrapper = mount(
+    render(
       <FilterHeaderRow
         {...commonFilterProps}
         ordering={[{ columnId: 'col1' }]}
         columns={[{ id: 'col1' }]}
       />
     );
-    wrapper.find('input').simulate('change', { target: { value: 'mytext' } });
-    wrapper.find('[title="Clear filter"]').simulate('click');
-    expect(wrapper.state()).toEqual({
-      filterValues: { col1: '' },
-      prevPropsFilters: [],
-    });
+    const input = screen.getByPlaceholderText('Type and hit enter to apply');
+    userEvent.type(input, 'mytext');
+    expect(input).toHaveValue('mytext');
+    userEvent.click(screen.getByTitle('Clear filter'));
+    expect(input).toHaveValue('');
   });
 
   it('filter input is hidden when isFilterable is false', () => {
-    const wrapper = mount(
+    render(
       <FilterHeaderRow
         {...commonFilterProps}
         ordering={[{ columnId: 'col1' }]}
         columns={[{ id: 'col1', isFilterable: false }]}
       />
     );
-    expect(wrapper.find('input').exists()).toEqual(false);
+    expect(screen.queryByPlaceholderText('Type and hit enter to apply')).not.toBeInTheDocument();
   });
 
   it('prevent filter modifications when isDisabled is true ', () => {
-    const wrapper = mount(
+    const { container } = render(
       <FilterHeaderRow
         {...commonFilterProps}
-        isDisabled="true"
+        isDisabled
         ordering={[{ columnId: 'col1' }, { columnId: 'col2' }]}
         columns={[{ id: 'col1' }, { id: 'col2', options: [{ id: 'opt1', text: 'option1' }] }]}
         filters={[{ columnId: 'col1', value: 'myVal' }]}
       />
     );
 
-    expect(wrapper.find(ComboBox).props().disabled).toEqual('true');
-    expect(wrapper.find(TextInput).props().disabled).toEqual('true');
-    expect(wrapper.find(`.${iotPrefix}--clear-filters-button--disabled`)).toHaveLength(1);
+    expect(screen.getByPlaceholderText('Type and hit enter to apply')).toHaveAttribute('disabled');
+    expect(screen.getByPlaceholderText('Choose an option')).toHaveAttribute('disabled');
+    expect(
+      container.querySelectorAll(`.${iotPrefix}--clear-filters-button--disabled`)
+    ).toHaveLength(1);
 
-    wrapper.find(`.${iotPrefix}--clear-filters-button--disabled`).simulate('click');
-    expect(wrapper.state().filterValues.col1).toEqual('myVal');
+    userEvent.click(screen.getByTitle('Clear filter'));
+    expect(screen.getByTitle('myVal')).toHaveValue('myVal');
   });
 
   it('adds an extra cell for the expander column when showExpanderColumn is true', () => {
