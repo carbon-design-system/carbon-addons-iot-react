5d889c9889a91f0c1ffe16e3af709f8ff30abb2d
Kevin Perrine <kperrine@gmail.com>
2021-09-01 13:47:50 -0400

    chore(simple-list): update story to make items configurable

--- packages/react/src/components/List/SimpleList/SimpleList.story.jsx
+++ packages/react/src/components/List/SimpleList/SimpleList.story.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Add16, Close16, Edit16 } from '@carbon/icons-react';
-import { boolean, select, text } from '@storybook/addon-knobs';
+import { boolean, select, text, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { spacing03 } from '@carbon/layout';
 import { Button, OverflowMenu, OverflowMenuItem } from 'carbon-components-react';
@@ -140,7 +140,7 @@ export const Basic = () => (
         pageOfPagesText: (pageNumber) => `Page ${pageNumber}`,
       }}
       buttons={buttonsToRender}
-      items={getListItems(30)}
+      items={getListItems(number('items to render', 30))}
       isFullHeight={boolean('isFullHeight', true)}
       pageSize={select('pageSize', ['sm', 'lg', 'xl'], 'xl')}
       isLoading={boolean('isLoading', false)}
