4f149a1f7dc0f2817d171bc490e13589fd4d16b4
David Conner <rdconner@us.ibm.com>
2021-09-23 19:32:29 -0400

    chore(test-app): trying to get process to exit

---
+++ packages/react/src/utils/serveTestApp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
+touch server.log
+
+npx serve -s --debug &>serve.log &
+NODE_PID=$!
+
+while read line; do
+  if [[ "$line" == *"Accepting connections"* ]]; then
+    echo "Accepting connections. Killing node."
+    kill $NODE_PID
+    exit 0
+  fi
+done <serve.log
+
+wait
