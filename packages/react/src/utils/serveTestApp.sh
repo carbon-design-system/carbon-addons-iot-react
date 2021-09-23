47cea0ee8fb775df7f947003665a11ff0ec94fd0
David Conner <rdconner@us.ibm.com>
2021-09-23 09:51:08 -0400

    chore(test-app): add serve script in separate file

---
+++ packages/react/src/utils/serveTestApp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
+npx serve -s ../../../test-app/build --debug &>serve.log &
+NODE_PID=$!
+
+while read line; do
+  if [[ "$line" == *"Accepting connections"* ]]; then
+    echo "Accepting connections. Killing node."
+    kill $NODE_PID
+    exit 0
+  fi
+done <serve.log
+
+wait
