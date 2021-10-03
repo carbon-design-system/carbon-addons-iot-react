4a4756b1de2a4e4cdb82cd12b1d197ce1c45fa16
Donisius Wigie <donisius.wigie@gmail.com>
2021-10-02 20:13:28 -0400

    fix(build): move entry points to the same level

--- packages/angular/src/side-panel/index.ts
+++
@@ -1,4 +0,0 @@
-export { SidePanel } from './side-panel.component';
-export { SidePanelTitleDirective } from './side-panel-title.directive';
-export { SidePanelFooterDirective } from './side-panel-footer.directive';
-export { SidePanelModule } from './side-panel.module';
