8568c78dc32309c8fe8ae49b7ce21fef080f01fa
Kevin Perrine <kperrine@gmail.com>
2021-10-18 14:04:13 -0400

    chore(table): update snapshots/jest config for TableMultiSortRow

--- packages/react/src/components/Table/TableMultiSortModal/TableMultSortModal.test.e2e.jsx
+++ packages/react/src/components/Table/TableMultiSortModal/TableMultSortModal.test.e2e.jsx
@@ -193,4 +193,99 @@ describe('TableMultiSortModal', () => {
         ]);
       });
   });
+
+  it('should not change the order when dragging over same row', () => {
+    const callbacks = {
+      onAddMultiSortColumn: cy.stub(),
+      onRemoveMultiSortColumn: cy.stub(),
+      onSaveMultiSortColumns: cy.stub(),
+      onCancelMultiSortColumns: cy.stub(),
+      onClearMultiSortColumns: cy.stub(),
+    };
+    mount(
+      <TableMultiSortModal
+        columns={[
+          {
+            id: 'string',
+            name: 'String',
+            isSortable: true,
+          },
+          {
+            id: 'select',
+            name: 'Select',
+            isSortable: true,
+          },
+          {
+            id: 'number',
+            name: 'Number',
+            isSortable: true,
+          },
+        ]}
+        ordering={[
+          {
+            columnId: 'string',
+            isHidden: false,
+          },
+          {
+            columnId: 'select',
+            isHidden: false,
+          },
+          {
+            columnId: 'number',
+            isHidden: false,
+          },
+        ]}
+        actions={callbacks}
+        sort={[
+          {
+            columnId: 'string',
+            direction: 'ASC',
+          },
+          {
+            columnId: 'select',
+            direction: 'DESC',
+          },
+          {
+            columnId: 'number',
+            direction: 'ASC',
+          },
+        ]}
+        showMultiSortModal
+        testId="multi_sort_modal"
+      />
+    );
+
+    cy.findByTestId(`drag-handle-string`)
+      .drag(`[data-testid="drag-handle-string"]`, { position: 'bottom' })
+      .should(() => {
+        cy.findByLabelText('Sort by').find('option:selected').should('have.text', 'String');
+        cy.findAllByLabelText('Then by')
+          .eq(0)
+          .find('option:selected')
+          .should('have.text', 'Select');
+        cy.findAllByLabelText('Then by')
+          .eq(1)
+          .find('option:selected')
+          .should('have.text', 'Number');
+      });
+
+    cy.findByRole('button', { name: 'Sort' })
+      .realClick()
+      .should(() => {
+        expect(callbacks.onSaveMultiSortColumns).to.have.been.calledWith([
+          {
+            columnId: 'string',
+            direction: 'ASC',
+          },
+          {
+            columnId: 'select',
+            direction: 'DESC',
+          },
+          {
+            columnId: 'number',
+            direction: 'ASC',
+          },
+        ]);
+      });
+  });
 });
