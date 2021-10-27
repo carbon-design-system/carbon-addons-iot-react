dcbdc64a86e82781426dc2636d5f0bb1976332bd
David Conner <rdconner@us.ibm.com>
2021-10-27 09:41:12 -0400

    chore(test-app): added script to change file paths from docker artifacts

---
+++ scripts/change-paths.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const coverage = JSON.parse(
+  fs.readFileSync(
+    '~/work/carbon-addons-iot-react/carbon-addons-iot-react/packages/react/cypress/coverage/coverage-final.json'
+  )
+);
+
+const fixedCoverage = {};
+Object.keys(coverage).forEach((key) => {
+  // /home/runner/work/carbon-addons-iot-react/carbon-addons-iot-react/packages/react/src/components/BarChartCard/BarChartCard.jsx
+  // /__w/carbon-addons-iot-react/carbon-addons-iot-react/packages/react/src/components/BarChartCard/BarChartCard.jsx
+  const fixedKey = key.replace('__w', 'home/runner/work');
+
+  const value = coverage[key];
+  value.path = fixedKey;
+  fixedCoverage[fixedKey] = value;
+});
+
+fs.writeFileSync('coverage-final-fixed.json', JSON.stringify(fixedCoverage));
