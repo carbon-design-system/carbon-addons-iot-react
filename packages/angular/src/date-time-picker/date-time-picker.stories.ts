823c2f85cd2c2bf028546f72d55a06eb6c742e6c
Fei <fei-fei.zheng@ibm.com>
2021-10-28 11:27:44 -0400

    fix(datetimepicker): fix preset relative story

--- packages/angular/src/date-time-picker/date-time-picker.stories.ts
+++ packages/angular/src/date-time-picker/date-time-picker.stories.ts
@@ -135,7 +135,7 @@ storiesOf('Components/DateTime Picker', module)
         null,
         {
           last: [5, 'WEEKS'],
-          relativeTo: ['YESTERDAY', '15:45'],
+          relativeTo: ['Yesterday', '15:45'],
         },
       ],
     }),
