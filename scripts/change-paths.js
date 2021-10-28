fb01b43e21d720ca3651ac44ac0c27fc659ac889
David Conner <rdconner@us.ibm.com>
2021-10-28 00:27:30 -0400

    chore(test-app): clean up script

--- scripts/change-paths.js
+++ scripts/change-paths.js
@@ -1,19 +1,14 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
 
 const coverage = JSON.parse(fs.readFileSync('./coverage-final.json'));
 
 const fixedCoverage = {};
 Object.keys(coverage).forEach((key) => {
-  // /home/runner/work/carbon-addons-iot-react/carbon-addons-iot-react/packages/react/src/components/BarChartCard/BarChartCard.jsx
-  // /__w/carbon-addons-iot-react/carbon-addons-iot-react/packages/react/src/components/BarChartCard/BarChartCard.jsx
   const fixedKey = key.replace('__w', 'home/runner/work');
-
   const value = coverage[key];
   value.path = fixedKey;
   fixedCoverage[fixedKey] = value;
-  console.log(fixedKey);
 });
 
 fs.writeFileSync('coverage-final.json', JSON.stringify(fixedCoverage));
