b97c8af079d84c5e777c5e822a9a4824e5a8207f
Kevin Perrine <kperrine@gmail.com>
2021-11-08 10:29:31 -0500

    fix(hierarchy-list): pr feedback to make label more clear

--- packages/react/src/components/List/HierarchyList/HierarchyList.story.jsx
+++ packages/react/src/components/List/HierarchyList/HierarchyList.story.jsx
@@ -471,9 +471,14 @@ export const WithLargeNumberOfItems = () => (
       items={[...Array(1000)].map((_, i) => ({
         id: `item-${i}`,
         content: {
-          value: i === 20 ? `Item ${i} `.repeat(i + 1 * 10) : `Item ${i}`,
+          value:
+            i === 20
+              ? `Item ${i} that has an extra long label that will definitely be truncated`
+              : `Item ${i}`,
           secondaryValue:
-            i === 10 ? `Item ${i} Subvalue `.repeat(i + 1 * 10) : `Item ${i} Subvalue`,
+            i === 10
+              ? `Item ${i} that has an extra long label that will definitely be truncated`
+              : `Item ${i} Subvalue`,
         },
       }))}
       editingStyle={EditingStyle.Single}
