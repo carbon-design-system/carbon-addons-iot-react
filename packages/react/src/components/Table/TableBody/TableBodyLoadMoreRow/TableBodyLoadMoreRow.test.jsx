30e34d5ca5608f9c52b2571a0254fbe978eb58cc
Shady Nessim <shady.nessim@ibm.com>
2022-01-27 17:37:36 -0500

    fix(table): test row to load in skeleton state
    3190

--- packages/react/src/components/Table/TableBody/TableBodyLoadMoreRow/TableBodyLoadMoreRow.test.jsx
+++ packages/react/src/components/Table/TableBody/TableBodyLoadMoreRow/TableBodyLoadMoreRow.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import TableBodyLoadMoreRow from './TableBodyLoadMoreRow';
 
-const tableRowProps = {
+const tableRowPropsNotLoading = {
   id: 'tableRow01',
   tableId: 'tableId',
   testId: 'table-test',
@@ -13,12 +13,21 @@ const tableRowProps = {
   isLoadingMore: false,
 };
 
+const tableRowPropsLoading = {
+  id: 'tableRow01',
+  tableId: 'tableId',
+  testId: 'table-test',
+  totalColumns: 5,
+  loadMoreText: 'Load more...',
+  isLoadingMore: true,
+};
+
 describe('TableBodyLoadMoreRow', () => {
   it('calls onRowLoadMore when load more is clicked', () => {
     const mockLoadMore = jest.fn();
     render(
       <TableBodyLoadMoreRow
-        {...tableRowProps}
+        {...tableRowPropsNotLoading}
         onRowLoadMore={mockLoadMore}
         options={{
           hasRowExpansion: true,
@@ -29,7 +38,26 @@ describe('TableBodyLoadMoreRow', () => {
       }
     );
     expect(mockLoadMore).not.toHaveBeenCalled();
-    userEvent.click(screen.getByRole('button', { name: tableRowProps.loadMoreText }));
-    expect(mockLoadMore).toHaveBeenCalledWith(tableRowProps.id);
+    userEvent.click(screen.getByRole('button', { name: tableRowPropsNotLoading.loadMoreText }));
+    expect(mockLoadMore).toHaveBeenCalledWith(tableRowPropsNotLoading.id);
+  });
+
+  it('renders a skeleton while loading', () => {
+    const mockLoadMore = jest.fn();
+    render(
+      <TableBodyLoadMoreRow
+        {...tableRowPropsLoading}
+        onRowLoadMore={mockLoadMore}
+        options={{
+          hasRowExpansion: true,
+        }}
+      />,
+      {
+        container: document.body.appendChild(document.createElement('tbody')),
+      }
+    );
+    expect(screen.getAllByTestId('table-test-tableRow01-skeleton').length).toEqual(
+      tableRowPropsLoading.totalColumns
+    );
   });
 });
