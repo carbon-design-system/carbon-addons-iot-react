f1384b025b9bb68e2af9b5282c3e3c12f5abd533
Marcelo Blechner <marcblec@br.ibm.com>
2022-02-11 12:51:05 -0300

    fix(IdleLogoutConfirmationModal): Disabling the "Stay Logged in" button after the Log out button is clicked.

--- packages/react/src/components/SuiteHeader/IdleLogoutConfirmationModal/IdleLogoutConfirmationModal.jsx
+++ packages/react/src/components/SuiteHeader/IdleLogoutConfirmationModal/IdleLogoutConfirmationModal.jsx
@@ -86,6 +86,8 @@ const IdleLogoutConfirmationModal = ({
   const [restartIdleTimer, setRestartIdleTimer] = useState(false);
   // Countdown state. If countdown is greater than zero, logout confirmation dialog is going to be displayed
   const [logoutConfirmationCountdown, setLogoutConfirmationCountdown] = useState(0);
+  // "Stay Logged in" button disabled state
+  const [stayLoggedInButtonDisabled, setStayLoggedInButtonDisabled] = useState(false);
   // eslint-disable-next-line consistent-return
   useEffect(() => {
     if (idleTimeoutData && idleTimeoutData?.timeout > 0 && routes) {
@@ -127,8 +129,11 @@ const IdleLogoutConfirmationModal = ({
       open={logoutConfirmationCountdown > 0}
       modalHeading={mergedI18N.sessionTimeoutModalHeading}
       primaryButtonText={mergedI18N.sessionTimeoutModalStayLoggedInButton}
+      primaryButtonDisabled={stayLoggedInButtonDisabled}
       secondaryButtonText={mergedI18N.sessionTimeoutModalLogoutButton}
       onSecondarySubmit={async () => {
+        // Disable the "Stay Logged in" button after the "Log out" button is clicked
+        setStayLoggedInButtonDisabled(true);
         const result = await onRouteChange(SUITE_HEADER_ROUTE_TYPES.LOGOUT, routes?.logout);
         if (result) {
           window.location.href = routes?.logout;
