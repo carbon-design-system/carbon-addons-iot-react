258418e5ffc59797d0e667372962d74328da69a8
David Conner <rdconner@us.ibm.com>
2022-07-21 20:55:44 -0400

    chore(timepicker): update snapshots

--- packages/react/src/components/TimePicker/ListSpinner.test.jsx
+++ packages/react/src/components/TimePicker/ListSpinner.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ListSpinner, { backwardArraySwap, forwardArraySwap, moveToSecondIndex } from './ListSpinner';
