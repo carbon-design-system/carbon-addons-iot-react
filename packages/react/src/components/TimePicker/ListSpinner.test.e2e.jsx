c2298adab25764178d213b3d9a69b60aef61b124
David Conner <rdconner@us.ibm.com>
2022-07-22 10:20:16 -0400

    chore(listspinner): moar test

--- packages/react/src/components/TimePicker/ListSpinner.test.e2e.jsx
+++ packages/react/src/components/TimePicker/ListSpinner.test.e2e.jsx
@@ -5,7 +5,7 @@ import { onlyOn } from '@cypress/skip-test';
 import ListSpinner from './ListSpinner';
 
 describe('ListSpinner', () => {
-  const listItems = Array.from(Array(12)).map((el, i) => {
+  const listItems = Array.from(Array(6)).map((el, i) => {
     const index = i + 1 < 10 ? `0${i + 1}` : `${i + 1}`;
     return { id: index, value: index };
   });
@@ -22,7 +22,38 @@ describe('ListSpinner', () => {
     });
   });
 
-  it('updates selected when scrolled', () => {
+  it('updates selected when scrolled down', () => {
+    const onClick = cy.stub().as('my-cb');
+    const commonProps = {
+      testId: 'my-list',
+      list: listItems,
+      defaultSelectedId: '04',
+      onClick,
+    };
+    mount(<ListSpinner {...commonProps} />);
+
+    cy.findByTestId('my-list-list').trigger('wheel', {
+      deltaX: 0,
+      deltaY: 1000,
+    });
+    cy.get('@my-cb').should('have.been.calledWith', '05');
+    cy.findByTestId('my-list-list').trigger('wheel', {
+      deltaX: 0,
+      deltaY: 1000,
+    });
+    cy.get('@my-cb').should('have.been.calledWith', '06');
+    cy.findByTestId('my-list-list').trigger('wheel', {
+      deltaX: 0,
+      deltaY: 1000,
+    });
+    cy.findByTestId('my-list-list').trigger('wheel', {
+      deltaX: 0,
+      deltaY: 1000,
+    });
+    cy.get('@my-cb').should('have.been.calledWith', '01');
+  });
+
+  it('updates selected when scrolled up', () => {
     const onClick = cy.stub().as('my-cb');
     const commonProps = {
       testId: 'my-list',
@@ -34,21 +65,27 @@ describe('ListSpinner', () => {
 
     cy.findByTestId('my-list-list').trigger('wheel', {
       deltaX: 0,
-      deltaY: 1000,
+      deltaY: -1000,
     });
+    cy.get('@my-cb').should('have.been.calledWith', '02');
     cy.findByTestId('my-list-list').trigger('wheel', {
       deltaX: 0,
       deltaY: -1000,
     });
-    cy.get('@my-cb').should('have.been.called');
+    cy.get('@my-cb').should('have.been.calledWith', '01');
+    cy.findByTestId('my-list-list').trigger('wheel', {
+      deltaX: 0,
+      deltaY: -1000,
+    });
+    cy.get('@my-cb').should('have.been.calledWith', '06');
   });
 
-  it('updates selected when swiped', () => {
+  it('updates selected when swiped down', () => {
     const onClick = cy.stub().as('my-cb2');
     const commonProps = {
       testId: 'my-list',
       list: listItems,
-      defaultSelectedId: '03',
+      defaultSelectedId: '02',
       onClick,
     };
     mount(<ListSpinner {...commonProps} />);
@@ -58,8 +95,29 @@ describe('ListSpinner', () => {
         touches: [{ pageY: 0, pageX: 0 }],
       })
       .trigger('touchmove', {
-        touches: [{ pageY: 1000, pageX: 0 }],
+        touches: [{ pageY: 500, pageX: 0 }],
       });
+    cy.get('@my-cb2').should('have.been.calledWith', '01');
+    cy.findByTestId('my-list-list')
+      .trigger('touchstart', {
+        touches: [{ pageY: 0, pageX: 0 }],
+      })
+      .trigger('touchmove', {
+        touches: [{ pageY: 500, pageX: 0 }],
+      });
+    cy.get('@my-cb2').should('have.been.calledWith', '06');
+  });
+
+  it('updates selected when swiped up', () => {
+    const onClick = cy.stub().as('my-cb2');
+    const commonProps = {
+      testId: 'my-list',
+      list: listItems,
+      defaultSelectedId: '05',
+      onClick,
+    };
+    mount(<ListSpinner {...commonProps} />);
+
     cy.findByTestId('my-list-list')
       .trigger('touchstart', {
         touches: [{ pageY: 1000, pageX: 0 }],
@@ -67,6 +125,14 @@ describe('ListSpinner', () => {
       .trigger('touchmove', {
         touches: [{ pageY: 0, pageX: 0 }],
       });
-    cy.get('@my-cb2').should('have.been.called');
+    cy.get('@my-cb2').should('have.been.calledWith', '06');
+    cy.findByTestId('my-list-list')
+      .trigger('touchstart', {
+        touches: [{ pageY: 1000, pageX: 0 }],
+      })
+      .trigger('touchmove', {
+        touches: [{ pageY: 0, pageX: 0 }],
+      });
+    cy.get('@my-cb2').should('have.been.calledWith', '01');
   });
 });
