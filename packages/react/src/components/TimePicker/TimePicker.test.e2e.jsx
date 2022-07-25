0406193e21b463dd843b481dcad21e49b2ed3682
David Conner <rdconner@us.ibm.com>
2022-07-25 10:55:53 -0400

    chore(timepicker): add test for dropdown placement

---
+++ packages/react/src/components/TimePicker/TimePicker.test.e2e.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+
+import TimePicker from './TimePicker';
+
+describe('TimePicker', () => {
+  it('renders dropdown above when on bottom of page', () => {
+    const onClick = cy.stub().as('my-cb');
+    const timePickerProps = {
+      readOnly: false,
+      hideLabel: false,
+      hideSecondaryLabel: false,
+      id: 'time-picker-test',
+      testId: 'time-picker-test',
+      i18n: {
+        labelText: 'Start',
+        secondaryLabelText: 'End',
+        helperText: 'This is some helper text',
+        warnText: 'You have been warned',
+      },
+      type: 'single',
+      invalid: [false, false],
+      warn: [false, false],
+      onChange: onClick,
+    };
+    mount(
+      <div style={{ paddingTop: `calc(90vh - 100px)` }}>
+        <TimePicker {...timePickerProps} />
+      </div>
+    );
+    cy.findByTestId('time-picker-test-input').then((pos) => {
+      cy.findByTestId('time-picker-test-time-btn')
+        .trigger('click')
+        .then(() => {
+          cy.findByTestId('time-picker-test-spinner').then((pos2) => {
+            expect(pos[0].getBoundingClientRect().top).to.be.equal(
+              pos2[0].getBoundingClientRect().bottom
+            );
+          });
+        });
+    });
+  });
+});
