2fa14e110783d599dcdeea36bb67c60baba3d4c3
Erzhan-Temir Mamyrov <Erzhan-Temir.Mamyrov@ibm.com>
2022-08-31 15:47:14 +0200

    chore(time picker spinner): move story to upper level
    #3517

--- packages/react/src/components/TimePickerSpinner/TimePickerSpinner.story.jsx
+++ packages/react/src/components/TimePickerSpinner/TimePickerSpinner.story.jsx
@@ -6,7 +6,7 @@ import { spacing05 } from '@carbon/layout';
 import TimePickerSpinner, { TIMEGROUPS } from './TimePickerSpinner';
 
 export default {
-  title: '1 - Watson IoT/Date time range selector/TimePickerSpinner',
+  title: '1 - Watson IoT/TimePickerSpinner',
 
   parameters: {
     component: TimePickerSpinner,
