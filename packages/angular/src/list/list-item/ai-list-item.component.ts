e3e9d2b454782fdad53e76dedb13a6ee68a8080c
Ravi Kumar <Ravi.Kumar70@ibm.com>
2023-02-22 18:38:56 +0530

    fix(listitem): removed hardcoded Item text

--- packages/angular/src/list/list-item/ai-list-item.component.ts
+++ packages/angular/src/list/list-item/ai-list-item.component.ts
@@ -78,7 +78,7 @@ import { IconService } from 'carbon-components-angular';
             [ngClass]="{ 'iot--list-item--content--values--main__large': item.size === 'lg' }"
           >
             <div
-              title="Item {{ item.value }}"
+              title="{{ item.value }}"
               class="iot--list-item--content--values--value"
               [ngClass]="{
                 'iot--list-item--category': item.isCategory,
