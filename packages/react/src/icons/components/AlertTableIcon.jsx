3302fd1979739a0d89c047f35357fe4c7ebd2e06
Amritha Raj Herle <herleraja@gmail.com>
2024-04-08 12:27:11 +0200

    chore: bump packages to address security vulnerabilities (#3852)
    * chore: bump packages to address security vulnerabilities
    
    * chore: updated browserslist
    
    * chore: bump non-major dependencies
    
    * chore: bump lerna package
    
    * revert: husky package version
    
    ---------
    
    Co-authored-by: Amritha raj herle <Amritha.Herle@ibm.com>

--- packages/react/src/icons/components/AlertTableIcon.jsx
+++ packages/react/src/icons/components/AlertTableIcon.jsx
@@ -1,32 +1,35 @@
 import * as React from 'react';
 
-function SvgAlertTableIcon(props) {
-  return (
-    <svg width={40} height={32} {...props}>
-      <defs>
-        <path
-          d="M14.94 13.26l-6.5-12a.5.5 0 00-.88 0l-6.5 12a.5.5 0 000 .5.5.5 0 00.44.24h13a.5.5 0 00.44-.74z"
-          id="alert-table-icon_svg__a"
-        />
-        <path
-          d="M8 10.5A.75.75 0 118 12a.75.75 0 010-1.5zm.565-4.995v4H7.44v-4h1.125z"
-          id="alert-table-icon_svg__b"
-        />
-      </defs>
-      <g fill="none" fillRule="evenodd">
-        <path fill="#0058FF" d="M0 0h40v2.759H0z" />
-        <path stroke="#005BFF" d="M.5 3.259h39V31.5H.5z" />
-        <path
-          fill="#D8D8D8"
-          d="M3.5 6.621h19V9.38h-19zm21.5 0h11V9.38H25zM3.5 18.759h19v2.759h-19zm0-6.069h19v2.759h-19zm21.5 0h11v2.759H25zM3.5 24.828h19v2.759h-19z"
-        />
-        <g transform="translate(21 13.714)">
-          <use fill="#0F62FE" xlinkHref="#alert-table-icon_svg__a" />
-          <use fill="#FFF" xlinkHref="#alert-table-icon_svg__b" />
-        </g>
+const SvgAlertTableIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width={40}
+    height={32}
+    {...props}
+  >
+    <defs>
+      <path
+        id="alert-table-icon_svg__a"
+        d="m14.94 13.26-6.5-12a.5.5 0 0 0-.88 0l-6.5 12a.5.5 0 0 0 0 .5.5.5 0 0 0 .44.24h13a.5.5 0 0 0 .44-.74"
+      />
+      <path
+        id="alert-table-icon_svg__b"
+        d="M8 10.5A.75.75 0 1 1 8 12a.75.75 0 0 1 0-1.5m.565-4.995v4H7.44v-4z"
+      />
+    </defs>
+    <g fill="none" fillRule="evenodd">
+      <path fill="#0058FF" d="M0 0h40v2.759H0z" />
+      <path stroke="#005BFF" d="M.5 3.259h39V31.5H.5z" />
+      <path
+        fill="#D8D8D8"
+        d="M3.5 6.621h19V9.38h-19zm21.5 0h11V9.38H25zM3.5 18.759h19v2.759h-19zm0-6.069h19v2.759h-19zm21.5 0h11v2.759H25zM3.5 24.828h19v2.759h-19z"
+      />
+      <g transform="translate(21 13.714)">
+        <use xlinkHref="#alert-table-icon_svg__a" fill="#0F62FE" />
+        <use xlinkHref="#alert-table-icon_svg__b" fill="#FFF" />
       </g>
-    </svg>
-  );
-}
-
+    </g>
+  </svg>
+);
 export default SvgAlertTableIcon;
