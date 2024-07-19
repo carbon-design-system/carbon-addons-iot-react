0a4b6c9b6354829cfa7404e0295bf9cc59f0e175
Abhishek Paul <abpaul24@in.ibm.com>
2024-07-19 12:32:44 +0530

    chore: fixed versioning issues

--- packages/react/src/components/Header/index.js
+++ packages/react/src/components/Header/index.js
@@ -1,13 +1,1 @@
-export default from './Header';
-export {
-  HeaderContainer,
-  HeaderGlobalAction,
-  HeaderGlobalBar,
-  HeaderMenu,
-  HeaderMenuButton,
-  HeaderMenuItem,
-  HeaderName,
-  HeaderNavigation,
-  HeaderPanel,
-  HeaderSideNavItems,
-} from '@carbon/react';
+export { HeaderGlobalAction, HeaderMenu, HeaderMenuItem, HeaderPanel } from '@carbon/react';
