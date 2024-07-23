b8a1c230c1a5c6624e561045a41509dc6493fc64
Hector Davis <hectord@br.ibm.com>
2024-07-23 16:51:03 -0300

    chore: page-title-bar style fix refactor

--- packages/react/src/components/PageTitleBar/PageTitleBar.jsx
+++ packages/react/src/components/PageTitleBar/PageTitleBar.jsx
@@ -4,8 +4,6 @@ import classnames from 'classnames';
 import { Information, Edit } from '@carbon/react/icons';
 import { Breadcrumb, BreadcrumbItem, Tooltip, SkeletonText, Tabs } from '@carbon/react';
 import { throttle } from 'lodash-es';
-import { rgba } from '@carbon/colors';
-import { g10 } from '@carbon/themes';
 
 import { settings } from '../../constants/Settings';
 import deprecate from '../../internal/deprecate';
@@ -353,14 +351,6 @@ const PageTitleBar = ({
         '--negative-header-offset': `-${headerOffsetCssVar}`,
         '--scroll-transition-progress':
           headerMode !== HEADER_MODES.DYNAMIC ? 1 : transitionProgress,
-        '--page-title-bar-background': rgba(
-          g10.layer01,
-          headerMode !== HEADER_MODES.DYNAMIC ? 1 : transitionProgress
-        ),
-        '--page-title-bar-border-bottom-color': rgba(
-          g10.layerAccent01,
-          headerMode !== HEADER_MODES.DYNAMIC ? 1 : transitionProgress
-        ),
         ...style,
       }}
     >
