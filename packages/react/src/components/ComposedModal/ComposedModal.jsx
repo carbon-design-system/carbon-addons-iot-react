cc0bf70266381564ee6bf2e1c349bb174b295ec6
Amritha raj herle <herleraja@gmail.com>
2025-03-12 18:01:06 +0100

    fix: change text input to number input

--- packages/react/src/components/ComposedModal/ComposedModal.jsx
+++ packages/react/src/components/ComposedModal/ComposedModal.jsx
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types';
 import React, { Fragment, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
 import warning from 'warning';
+import { v4 as uuidv4 } from 'uuid';
 
 import { settings } from '../../constants/Settings';
 import { scrollErrorIntoView } from '../../utils/componentUtilityFunctions';
@@ -235,6 +236,7 @@ const ComposedModal = ({
       ) : null}
       {error ? (
         <InlineNotification
+          key={uuidv4()} // regenerate the inline notification after they click on close button
           title={error}
           subtitle=""
           kind="error"
