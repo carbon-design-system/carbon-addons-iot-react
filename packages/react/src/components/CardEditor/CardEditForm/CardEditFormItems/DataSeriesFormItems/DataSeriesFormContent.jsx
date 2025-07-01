d5950a32406efb56e6e8d837d9763314d1507d5d
Tirth Patel <tirth.patel1@ibm.com>
2025-07-01 13:25:08 +0530

    fix: update remove icon for card editor

--- packages/react/src/components/CardEditor/CardEditForm/CardEditFormItems/DataSeriesFormItems/DataSeriesFormContent.jsx
+++ packages/react/src/components/CardEditor/CardEditForm/CardEditFormItems/DataSeriesFormItems/DataSeriesFormContent.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo, useCallback, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Edit, Subtract } from '@carbon/react/icons';
+import { Edit, MisuseOutline } from '@carbon/react/icons';
 import { omit, isEmpty } from 'lodash-es';
 import { v4 as uuidv4 } from 'uuid';
 import hash from 'object-hash';
@@ -419,7 +419,7 @@ const DataSeriesFormItem = ({
               />,
               <Button
                 key={`data-item-${dataItem.dataSourceId}_remove`}
-                renderIcon={Subtract}
+                renderIcon={MisuseOutline}
                 hasIconOnly
                 kind="ghost"
                 size="sm"
