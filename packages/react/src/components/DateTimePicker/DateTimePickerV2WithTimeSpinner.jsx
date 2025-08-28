76cb1cda70c427d81913fe868f145213b3c94f6a
Anish <anishkumar.bhut@ibm.com>
2025-08-28 16:11:46 +0530

    lint fixed

--- packages/react/src/components/DateTimePicker/DateTimePickerV2WithTimeSpinner.jsx
+++ packages/react/src/components/DateTimePicker/DateTimePickerV2WithTimeSpinner.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useMemo, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import {
