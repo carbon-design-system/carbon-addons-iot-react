3d2627e686292be6220f7afe301c29b2bc0636c5
Anishkumar Bhut <Anishkumar.bhut@ibm.com>
2025-12-03 16:39:17 +0530

    revert this unwanted

--- packages/react/src/components/Table/TableBody/RowActionsCell/RowActionsCell.jsx
+++ packages/react/src/components/Table/TableBody/RowActionsCell/RowActionsCell.jsx
@@ -193,7 +193,7 @@ class RowActionsCell extends React.Component {
                     onOpen={this.handleOpen}
                     onClose={this.handleClose}
                     // compact or xs rows need the `sm` overflow menu, everything else is default (md)
-                    size={['compact', 'md'].includes(size) ? 'sm' : 'md'}
+                    size={['compact', 'sx'].includes(size) ? 'sm' : 'md'}
                     className={`${iotPrefix}--row-actions-cell--overflow-menu`}
                     selectorPrimaryFocus={`.${iotPrefix}--action-overflow-item--initialFocus`}
                     useAutoPositioning
