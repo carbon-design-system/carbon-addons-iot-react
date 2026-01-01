fdb45e86d58c4b8e34ab83f8f9dc5c617344f830
Vivek Domadiya <45758448+vivekdomadiya@users.noreply.github.com>
2026-01-01 19:22:59 +0530

    feat: add disableDataFilter option to timeseries and value card (#4053)
    MASMON-4472

--- packages/react/src/components/CardEditor/CardEditForm/CardEditFormItems/DataSeriesFormItemModal.jsx
+++ packages/react/src/components/CardEditor/CardEditForm/CardEditFormItems/DataSeriesFormItemModal.jsx
@@ -301,9 +301,21 @@ const DataSeriesFormItemModal = ({
   const initialAggregation = matchedDataItem?.aggregationMethod;
   const initialGrain = matchedDataItem?.grain;
 
-  const selectedDimensionFilter = editDataItem.dataFilter
-    ? Object.keys(editDataItem.dataFilter)[0]
-    : '';
+  const selectedDimensionFilter = useMemo(
+    () => (editDataItem.dataFilter ? Object.keys(editDataItem.dataFilter)[0] : ''),
+    [editDataItem.dataFilter]
+  );
+
+  const availableDimensionsItems = useMemo(
+    () =>
+      (selectedDimensionFilter &&
+        availableDimensions[selectedDimensionFilter]?.sort().map((item) => ({
+          id: item,
+          text: item.toString(),
+        }))) ||
+      [],
+    [availableDimensions, selectedDimensionFilter]
+  );
 
   const DataEditorContent = useMemo(
     () => (
@@ -535,32 +547,29 @@ const DataSeriesFormItemModal = ({
               </div>
 
               <div className={`${baseClassName}--input-group--item`}>
-                {!isEmpty(editDataItem.dataFilter) &&
-                  availableDimensions[selectedDimensionFilter] && (
-                    <Dropdown
-                      id={`${id}_data-filter-value`}
-                      label=""
-                      direction="bottom"
-                      items={availableDimensions[selectedDimensionFilter]?.sort()}
-                      light
-                      itemToString={(item) => item?.toString()}
-                      selectedItem={
-                        editDataItem.dataFilter
-                          ? editDataItem.dataFilter[selectedDimensionFilter]
-                          : undefined
-                      }
-                      onChange={({ selectedItem }) => {
-                        const dataFilter = {
-                          [selectedDimensionFilter]: selectedItem,
-                        };
-                        setEditDataItem({
-                          ...editDataItem,
-                          dataFilter,
-                        });
-                      }}
-                      autoAlign
-                    />
-                  )}
+                {!isEmpty(editDataItem.dataFilter) && availableDimensionsItems?.length > 0 && (
+                  <Dropdown
+                    id={`${id}_data-filter-value`}
+                    label=""
+                    direction="bottom"
+                    items={availableDimensionsItems}
+                    light
+                    itemToString={(item) => (item ? item.text : '')}
+                    selectedItem={availableDimensionsItems.find(
+                      (item) => item.id === editDataItem.dataFilter?.[selectedDimensionFilter]
+                    )}
+                    onChange={({ selectedItem }) => {
+                      const dataFilter = {
+                        [selectedDimensionFilter]: selectedItem?.id,
+                      };
+                      setEditDataItem({
+                        ...editDataItem,
+                        dataFilter,
+                      });
+                    }}
+                    autoAlign
+                  />
+                )}
               </div>
             </div>
           )}
@@ -588,6 +597,7 @@ const DataSeriesFormItemModal = ({
     ),
     [
       availableDimensions,
+      availableDimensionsItems,
       availableGrains,
       baseClassName,
       cardConfig,
